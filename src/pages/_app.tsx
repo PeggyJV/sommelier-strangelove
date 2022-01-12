d9e26ce08dd206cc8ee9b5cda9f94cb7c3aa40f4
Xander Low <xander@xandydandy.com>
2022-01-12 14:54:50 -0800

    configured react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,22 @@ import { DialogProvider } from 'context/dialogContext'
 import type { AppProps } from 'next/app'
 import PlausibleProvider from 'next-plausible'
 import theme from 'theme/index'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
+  const queryClient = new QueryClient()
+
   return (
-    <PlausibleProvider domain={process.env.NEXT_PUBLIC_PLAUSIBLE_URL!}>
-      <ChakraProvider theme={theme}>
-        <DialogProvider>
-          <Component {...pageProps} />
-        </DialogProvider>
-      </ChakraProvider>
-    </PlausibleProvider>
+    <QueryClientProvider client={queryClient}>
+      <PlausibleProvider domain={process.env.NEXT_PUBLIC_PLAUSIBLE_URL!}>
+        <ChakraProvider theme={theme}>
+          <DialogProvider>
+            <Component {...pageProps} />
+          </DialogProvider>
+        </ChakraProvider>
+      </PlausibleProvider>
+    </QueryClientProvider>
   )
 }
+
+export default App
