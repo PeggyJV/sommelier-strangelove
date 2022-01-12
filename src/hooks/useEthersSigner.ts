c7cb6d65098747e13966c1bda0f51d7041568d6f
Xander Low <xander@xandydandy.com>
2022-01-12 15:29:43 -0800

    chore: solve typescript error

---
+++ src/hooks/useEthersSigner.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@chakra-ui/react'
+import { ethers } from 'ethers'
+
+const useEthersSigner = () =>
+  useQuery('ethers', () => {
+    const ethereum = window.ethereum
+    const provider = new ethers.providers.Web3Provider(ethereum)
+    const signer = provider.getSigner()
+
+    return signer
+  })
+
+export default useEthersSigner
