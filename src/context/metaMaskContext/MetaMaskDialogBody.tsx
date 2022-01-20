c8c3764d7d43299db340e1a347c99896f29326bc
Xander Low <alexanderjameslow@gmail.com>
2022-01-20 10:22:58 -0800

    removed: unnecessary fragment

--- src/context/metaMaskContext/MetaMaskDialogBody.tsx
+++ src/context/metaMaskContext/MetaMaskDialogBody.tsx
@@ -3,16 +3,14 @@ import { Text, Link } from '@chakra-ui/react'
 
 const MetaMaskDialogBody: FC = () => {
   return (
-    <>
-      <Text>
-        We use MetaMask to handle authentication and the delegation of funds on
-        our platform. Please ensure you're using a supported brower and install
-        their extension{' '}
-        <Link href='https://metamask.io/download/' isExternal>
-          here.
-        </Link>
-      </Text>
-    </>
+    <Text>
+      We use MetaMask to handle authentication and the delegation of funds on
+      our platform. Please ensure you're using a supported brower and install
+      their extension{' '}
+      <Link href='https://metamask.io/download/' isExternal>
+        here.
+      </Link>
+    </Text>
   )
 }
 
