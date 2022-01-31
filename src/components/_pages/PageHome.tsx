97f953f7cf6037fddb60e0a5d70e2f56791a0d1a
Griko Nibras <git@griko.id>
2022-02-01 03:16:32 +0700

    refactor(pages): update home example with wagmi
    This commit replaces and improves the initial connect button with an exhaustive example on using `wagmi` for connecting and disconnecting.

--- src/components/_pages/PageHome.tsx
+++ src/components/_pages/PageHome.tsx
@@ -1,18 +1,67 @@
 import { NextPage } from 'next'
-import { Button, Container, Heading } from '@chakra-ui/react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Stack,
+  Text
+} from '@chakra-ui/react'
 import Layout from 'components/Layout'
-import useConnectWallet from 'hooks/useConnectWallet'
+import { useAccount, useConnect } from 'wagmi'
+import ClientOnly from 'components/ClientOnly'
+import { getConnectorScheme } from 'src/utils/chakra'
 
 const PageHome: NextPage = () => {
-  const { connectToWallet } = useConnectWallet()
+  const [account, disconnect] = useAccount({
+    fetchEns: true
+  })
+  const [auth, connect] = useConnect()
 
   return (
     <Layout>
       <Container maxW='container.lg'>
         <Heading>Welcome Home</Heading>
-        <Button colorScheme='orange' onClick={connectToWallet}>
-          Connect with MetaMask
-        </Button>
+
+        {auth.data.connected && account.data && (
+          <HStack p={4} rounded='md' borderWidth={1} spacing={4}>
+            <Avatar />
+            <Stack flexGrow={1} spacing={1}>
+              <Text fontWeight='bold' isTruncated>
+                {account.data.address}
+              </Text>
+              <Text color='gray.500'>
+                Connected to {account.data.connector?.name}
+              </Text>
+            </Stack>
+            <Button
+              colorScheme='red'
+              onClick={disconnect}
+              isLoading={account.loading}
+            >
+              Disconnect
+            </Button>
+          </HStack>
+        )}
+
+        {!auth.data.connected && (
+          <ClientOnly>
+            {auth.data.connectors.map(c => (
+              <Button
+                colorScheme={getConnectorScheme(c.name)}
+                isDisabled={!c.ready}
+                isLoading={auth.loading}
+                key={c.id}
+                onClick={() => connect(c)}
+              >
+                Connect with {c.name}
+                {!c.ready && ' (unsupported)'}
+              </Button>
+            ))}
+          </ClientOnly>
+        )}
       </Container>
     </Layout>
   )
