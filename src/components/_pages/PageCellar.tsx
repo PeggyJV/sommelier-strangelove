01f61a447fbc0a7698b7057e99661e3fbf91fe76
Xander Low <alexanderjameslow@gmail.com>
2022-02-17 17:21:50 -0800

    added: temp cellar routes

---
+++ src/components/_pages/PageCellar.tsx
@@ -0,0 +1,7 @@
+import Layout from 'components/Layout'
+
+const PageCellar = () => {
+  return <Layout>PageCellar</Layout>
+}
+
+export default PageCellar
---
+++ src/pages/cellars/cellar.tsx
@@ -0,0 +1,7 @@
+import PageCellar from 'components/_pages/PageCellar'
+
+const CellarPage = () => {
+  return <PageCellar />
+}
+
+export default CellarPage
