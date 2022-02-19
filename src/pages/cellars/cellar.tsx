c1e68ebae1b43b4a1b5c57b554e6a7781fda26a1
Xander Low <alexanderjameslow@gmail.com>
2022-02-18 16:50:30 -0800

    added: victory

--- src/pages/cellars/cellar.tsx
+++ src/pages/cellars/cellar.tsx
@@ -1,6 +1,7 @@
 import PageCellar from 'components/_pages/PageCellar'
+import { NextPage } from 'next'
 
-const CellarPage = () => {
+const CellarPage: NextPage = () => {
   return <PageCellar />
 }
 
