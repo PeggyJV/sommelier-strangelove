43ee97396e32c2a942804a204cee269593194de6
Xander Low <xander@xandydandy.com>
2022-02-28 16:58:48 -0800

    refactored: copy and ui updates to match design

--- src/components/_pages/PageHome.tsx
+++ src/components/_pages/PageHome.tsx
@@ -1,17 +1,11 @@
 import { NextPage } from 'next'
-import {
-  Box,
-  Button,
-  Flex,
-  Grid,
-  Heading,
-  Text,
-  VStack
-} from '@chakra-ui/react'
+import { Box, Flex, Grid, Heading, Img, Text, VStack } from '@chakra-ui/react'
 import Layout from 'components/Layout'
 import { CellarOverviewCard } from 'components/_cards/CellarOverviewCard'
 import { useConnect } from 'wagmi'
 import { Card } from 'components/_cards/Card'
+import { BaseButton } from 'components/_buttons/BaseButton'
+import { FaArrowRight } from 'react-icons/fa'
 
 const PageHome: NextPage = () => {
   const [auth] = useConnect()
@@ -21,25 +15,30 @@ const PageHome: NextPage = () => {
   return (
     <Layout>
       <VStack spacing={6} align='flex-start'>
-        <Grid templateColumns='1fr 1fr' gap={6} alignItems='center'>
+        <Grid
+          as='section'
+          pb={10}
+          templateColumns='1fr 1fr'
+          gap={6}
+          justifyItems='center'
+        >
           <VStack align='flex-start' spacing={4} maxW='70ch'>
             <Heading>Sommelier DeFi</Heading>
             <Text>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo nemo
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga.
             </Text>
-            <Button px={14} bg='white' color='black'>
-              Explore More
-            </Button>
+            <BaseButton icon={FaArrowRight}>Learn More</BaseButton>
           </VStack>
-          <Box h={330} bg='gray.800' />
+          <Img src='/placeholders/img-placeholder.png' />
         </Grid>
         <Flex w='100%' as='section' direction='column'>
           <Box pb={4}>
             <Heading>Cellars</Heading>
-            <Text color='whiteAlpha.800'>
+            <Text maxW='70ch'>
               At vero eos et accusamus et iusto odio dignissimos ducimus qui
               blanditiis praesentium voluptatum deleniti atque corrupti quos
               dolores et quas molestias excepturi sint.
@@ -47,24 +46,23 @@ const PageHome: NextPage = () => {
           </Box>
           <Grid gap={6} templateColumns='1fr 1fr'>
             <CellarOverviewCard isConnected={isConnected} />
-            <Card
-              display='flex'
-              flexDir='column'
-              justifyContent='center'
-              alignItems='center'
-              textAlign='center'
-              bgColor='violentViolet.900'
-            >
-              <Flex direction='column' align='center'>
+            <Card bgColor='darker' textAlign='center'>
+              <VStack spacing={4}>
+                <Img
+                  src='/placeholders/img-placeholder.png'
+                  boxSize={40}
+                  objectFit='contain'
+                  pb={4}
+                />
                 <Text fontSize='2xl' fontWeight='medium'>
                   More Coming Soon
                 </Text>
-                <Text color='whiteAlpha.800' pb={6}>
+                <Text pb={6}>
                   At vero eos et accusamus et iusto odio dignissimos ducimus qui
                   blanditiis praesentium voluptatum deleniti atque corrupti quos
                   dolores et quas molestias excepturi sint.
                 </Text>
-              </Flex>
+              </VStack>
             </Card>
           </Grid>
         </Flex>
