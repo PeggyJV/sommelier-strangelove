84808ea9a14941dba14d4abda37031895c0d142d
Xander Low <alexanderjameslow@gmail.com>
2022-03-04 13:25:20 -0800

    removed: temp selecto

--- src/components/Nav/TopNav.tsx
+++ src/components/Nav/TopNav.tsx
@@ -1,27 +1,19 @@
 import { VFC } from 'react'
-import { Flex, HStack, Text } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
 import { useConnect } from 'wagmi'
 import ConnectButton from 'components/_buttons/ConnectButton'
-import { ChainSelector } from 'components/ChainSelector'
-
-const placeholderChains = ['Ethereum', 'Cosmos']
 
 export const TopNav: VFC = () => {
   const [auth] = useConnect()
-
-  const isConnected = auth.data.connected
 
   return (
     <Flex flex={1} py={8} px={6} fontSize='xl' justify='space-between'>
       <Text fontFamily='brand' color='light'>
         Overview
       </Text>
-      <HStack spacing={4}>
-        {isConnected && <ChainSelector chains={placeholderChains} />}
-        {auth.data.connectors.map(c => (
-          <ConnectButton connector={c} key={c.id} />
-        ))}
-      </HStack>
+      {auth.data.connectors.map(c => (
+        <ConnectButton connector={c} key={c.id} />
+      ))}
     </Flex>
   )
 }
