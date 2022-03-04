2b40d556c0c9fc4e8ae146549e1ded9febe043ba
Xander Low <alexanderjameslow@gmail.com>
2022-03-04 13:50:37 -0800

    refactored: side nav footer links

--- src/components/Nav/SideNav/index.tsx
+++ src/components/Nav/SideNav/index.tsx
@@ -14,34 +14,51 @@ import { Card } from 'components/_cards/Card'
 
 export const SideNav = () => {
   return (
-    <VStack as='nav' px={6} py={8} align='flex-start'>
-      <List fontFamily='brand' pb={16}>
-        {links.map((link, i) => {
-          const { href, title } = link
-          return (
-            <Link key={i} href={href}>
-              <ListItem>
-                <ListIcon as={BsDiamondFill} color='electricIndigo.400' />
-                {title}
-              </ListItem>
-            </Link>
-          )
-        })}
-      </List>
-      <Card minW='28ch' bg='violentViolet'>
-        <VStack align='flex-start'>
-          <Text fontFamily='brand'>Join Our Community</Text>
-          <Text pb={6}>
-            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-            consectetur.
-          </Text>
-          <HStack spacing={6}>
-            <Icon as={BsFillSquareFill} boxSize={6} color='brilliantRose.500' />
-            <Icon as={BsCircleFill} boxSize={6} color='deepSkyBlue.400' />
-            <Icon as={BsDiamondFill} boxSize={6} color='electricIndigo.400' />
-          </HStack>
-        </VStack>
-      </Card>
+    <VStack h='100%' px={6} align='stretch' justify='space-between'>
+      <VStack as='nav' py={8} align='flex-start'>
+        <List fontFamily='brand' pb={16}>
+          {links.map((link, i) => {
+            const { href, title } = link
+            return (
+              <Link key={i} href={href}>
+                <ListItem>
+                  <ListIcon as={BsDiamondFill} color='electricIndigo.400' />
+                  {title}
+                </ListItem>
+              </Link>
+            )
+          })}
+        </List>
+        <Card minW='28ch' bg='violentViolet'>
+          <VStack align='flex-start'>
+            <Text fontFamily='brand'>Join Our Community</Text>
+            <Text pb={6}>
+              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+              consectetur.
+            </Text>
+            <HStack spacing={6}>
+              <Icon
+                as={BsFillSquareFill}
+                boxSize={6}
+                color='brilliantRose.500'
+              />
+              <Icon as={BsCircleFill} boxSize={6} color='deepSkyBlue.400' />
+              <Icon as={BsDiamondFill} boxSize={6} color='electricIndigo.400' />
+            </HStack>
+          </VStack>
+        </Card>
+      </VStack>
+      <HStack
+        spacing={4}
+        py={8}
+        justify='center'
+        borderTop='2px solid'
+        borderColor='backgrounds.dark'
+      >
+        <Link href='/'>Documentation</Link>
+        <Link href='/'>Terms</Link>
+        <Link href='/'>Privacy</Link>
+      </HStack>
     </VStack>
   )
 }
