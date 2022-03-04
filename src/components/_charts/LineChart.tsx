2f83ead1f260d8aa72bdd4ba5bf972a5f4f19ed9
Xander Low <xander@xandydandy.com>
2022-03-03 18:18:47 -0800

    refactored: abstracted line chart defaults for different styles

--- src/components/_charts/LineChart.tsx
+++ src/components/_charts/LineChart.tsx
@@ -1,8 +1,7 @@
 import { VFC } from 'react'
-import { linearGradientDef } from '@nivo/core'
 import { ResponsiveLine, LineSvgProps } from '@nivo/line'
 import { useTheme } from '@chakra-ui/react'
-import { ToolTip } from './ToolTip'
+import { ToolTip } from './LineToolTip'
 
 const LineChart: VFC<LineSvgProps> = ({ data, ...rest }) => {
   const { colors } = useTheme()
@@ -10,13 +9,6 @@ const LineChart: VFC<LineSvgProps> = ({ data, ...rest }) => {
   return (
     <ResponsiveLine
       data={data}
-      defs={[
-        linearGradientDef('gradient', [
-          { offset: 70, color: 'inherit' },
-          { offset: 100, color: 'inherit', opacity: 0 }
-        ])
-      ]}
-      fill={[{ match: '*', id: 'gradient' }]}
       useMesh
       enableGridX={false}
       enableGridY={false}
@@ -25,7 +17,6 @@ const LineChart: VFC<LineSvgProps> = ({ data, ...rest }) => {
         clamp: true,
         duration: 220
       }}
-      enableArea
       enablePoints={false}
       colors={[colors.brilliantRose[400]]}
       {...rest}
