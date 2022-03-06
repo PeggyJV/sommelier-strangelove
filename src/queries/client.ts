578a0697e4a99ce977a3b3faa729ccdd601901e0
Elliot Shiu <sandbochs@users.noreply.github.com>
2022-03-06 23:24:07 +0900

    wire up urql provider

---
+++ src/queries/client.ts
@@ -0,0 +1,6 @@
+import { createClient } from 'urql'
+
+export const client = createClient({
+  // TODO: Pull this from config / env
+  url: 'https://api.thegraph.com/subgraphs/name/elkdao/cellars'
+})
