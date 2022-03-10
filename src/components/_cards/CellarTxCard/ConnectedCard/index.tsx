effd9f2f51a1eaa166d6eb4948db1d0fc1ef0145
Xander Low <alexanderjameslow@gmail.com>
2022-03-10 14:26:45 -0800

    refactor: abstracted handlers to custom hook

--- src/components/_cards/CellarTxCard/ConnectedCard/index.tsx
+++ src/components/_cards/CellarTxCard/ConnectedCard/index.tsx
@@ -11,8 +11,7 @@ import {
 } from '@chakra-ui/react'
 import { BaseButton } from 'components/_buttons/BaseButton'
 import { CardHeading } from 'components/_typography/CardHeading'
-import { useBrandedToast } from 'hooks/chakra'
-import { useState, VFC } from 'react'
+import { VFC } from 'react'
 import { FaEthereum } from 'react-icons/fa'
 import { Card } from 'components/_cards/Card'
 import {
@@ -22,71 +21,10 @@ import {
   tabProps
 } from './styles'
 import { TxInput } from './TxInput'
-import { ImCheckmark } from 'react-icons/im'
-import { AiOutlineInfo } from 'react-icons/ai'
-import { BiError } from 'react-icons/bi'
+import { useSubmitTx } from 'hooks/useSubmitTx'
 
 export const ConnectedCard: VFC = () => {
-  const [isDisabled, setDisabled] = useState<boolean>(false)
-  const [loading, setLoading] = useState<boolean>(false)
-  const { addToast, update, close, closeAll } = useBrandedToast()
-
-  // placeholder submit handler
-  const handleDepositSubmit = () => {
-    setDisabled(true)
-    setLoading(true)
-    addToast({
-      body: <Text>Depositing 10.125 ETH into Cellar</Text>,
-      isLoading: true,
-      closeHandler: close,
-      duration: null
-    })
-    setTimeout(() => {
-      setDisabled(false)
-      setLoading(false)
-      update({
-        body: <Text>12345.678 ETH deposited into Cellar View on ETHScan</Text>,
-        status: 'success',
-        closeHandler: closeAll,
-        icon: ImCheckmark
-      })
-      addToast({
-        body: (
-          <VStack align='flex-start'>
-            <Text>
-              You now have 12345.678 LP tokens available in this cellar. Lock
-              your LP tokens for certain period of time to earn SOMM token
-              rewards.
-            </Text>
-            <BaseButton variant='solid'>Start Earning</BaseButton>
-          </VStack>
-        ),
-        duration: null,
-        icon: AiOutlineInfo
-      })
-    }, 2000)
-  }
-
-  const handleWithdrawSubmit = () => {
-    setDisabled(true)
-    setLoading(true)
-    addToast({
-      body: <Text>Withdrawing 10.125 ETH from Cellar</Text>,
-      isLoading: true,
-      closeHandler: close,
-      duration: null
-    })
-    setTimeout(() => {
-      setDisabled(false)
-      setLoading(false)
-      update({
-        body: <Text>Sorry, something wen't wrong with this withdrawal. Please, try again.</Text>,
-        status: 'error',
-        closeHandler: closeAll,
-        icon: BiError
-      })
-    }, 2000)
-  }
+  const { isDisabled, loading, handleDeposit, handleWithdraw } = useSubmitTx()
 
   return (
     <Card
@@ -124,7 +62,7 @@ export const ConnectedCard: VFC = () => {
               <BaseButton
                 isLoading={loading}
                 disabled={isDisabled}
-                onClick={handleDepositSubmit}
+                onClick={handleDeposit}
                 {...disabledButtonProps}
               >
                 Add Deposit
@@ -153,7 +91,7 @@ export const ConnectedCard: VFC = () => {
               <BaseButton
                 isLoading={loading}
                 disabled={isDisabled}
-                onClick={handleWithdrawSubmit}
+                onClick={handleWithdraw}
                 {...disabledButtonProps}
               >
                 Withdraw
