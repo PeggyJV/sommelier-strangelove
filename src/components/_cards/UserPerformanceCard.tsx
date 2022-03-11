6aab5c4228f90924f5ba9b903e717af376686b10
Xander Low <alexanderjameslow@gmail.com>
2022-03-10 17:02:05 -0800

    added: bg colors

--- src/components/_cards/UserPerformanceCard.tsx
+++ src/components/_cards/UserPerformanceCard.tsx
@@ -7,40 +7,42 @@ import { BsCurrencyDollar } from 'react-icons/bs'
 
 export const UserPerformanceCard: VFC<BoxProps> = () => {
   return (
-    <Card>
-      <CardStatRow>
-        <CardStat
-          label='your principal'
-          labelIcon=''
-          stat='12.5K USDC'
-          statIcon={BsCurrencyDollar}
-        />
-        <CardStat
-          label='your net asset value'
-          labelIcon=''
-          stat='23.34K USDC'
-          statIcon={BsCurrencyDollar}
-        />
-        <CardStat label='your yield' labelIcon='' stat='3.7%' />
-        <CardStat
-          label='your rewards'
-          labelIcon=''
-          stat='0.28K USDC'
-          statIcon={BsCurrencyDollar}
-        />
-        <CardStat
-          label='lp tokens'
-          labelIcon=''
-          stat='0.28K USDC'
-          statIcon={BsCurrencyDollar}
-        />
-        <CardStat
-          label='your bonded total'
-          labelIcon=''
-          stat='10.25 USDC'
-          statIcon={BsCurrencyDollar}
-        />
-      </CardStatRow>
+    <Card bg='backgrounds.dark'>
+      <Card bg='backgrounds.darker'>
+        <CardStatRow>
+          <CardStat
+            label='your principal'
+            labelIcon=''
+            stat='12.5K USDC'
+            statIcon={BsCurrencyDollar}
+          />
+          <CardStat
+            label='your net asset value'
+            labelIcon=''
+            stat='23.34K USDC'
+            statIcon={BsCurrencyDollar}
+          />
+          <CardStat label='your yield' labelIcon='' stat='3.7%' />
+          <CardStat
+            label='your rewards'
+            labelIcon=''
+            stat='0.28K USDC'
+            statIcon={BsCurrencyDollar}
+          />
+          <CardStat
+            label='lp tokens'
+            labelIcon=''
+            stat='0.28K USDC'
+            statIcon={BsCurrencyDollar}
+          />
+          <CardStat
+            label='your bonded total'
+            labelIcon=''
+            stat='10.25 USDC'
+            statIcon={BsCurrencyDollar}
+          />
+        </CardStatRow>
+      </Card>
     </Card>
   )
 }
