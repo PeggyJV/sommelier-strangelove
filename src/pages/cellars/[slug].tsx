568c71ebaa6c22ce9485b7ce2d836b46c4f78ff0
Xander Low <xander@xandydandy.com>
2022-03-16 15:40:13 -0700

    refactored: interfaces and types

--- src/pages/cellars/[slug].tsx
+++ src/pages/cellars/[slug].tsx
@@ -1,20 +1,22 @@
 import PageCellar from 'components/_pages/PageCellar'
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import { GetAllCellarsDocument } from 'generated/subgraph'
+import { GetCellarRoutesDocument } from 'generated/subgraph'
 import { ssrClient } from 'queries/client'
 import { ParsedUrlQuery } from 'querystring'
 import slugify from 'slugify'
 
-interface Params extends ParsedUrlQuery {
+export interface CellarPageProps {
   slug: string
 }
+
+type Params = ParsedUrlQuery & CellarPageProps
 
 const CellarPage: NextPage = () => {
   return <PageCellar />
 }
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
-  const { data } = await ssrClient.query(GetAllCellarsDocument).toPromise()
+  const { data } = await ssrClient.query(GetCellarRoutesDocument).toPromise()
   const { cellars } = data
   const paths = cellars.map((cellar: any) => {
     const { name } = cellar
