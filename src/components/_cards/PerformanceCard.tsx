59596b0e65688184ed90a95358c4d48c23ca01ef
Xander Low <xander@xandydandy.com>
2022-03-16 16:51:13 -0700

    refactored: copy and added legend

--- src/components/_cards/PerformanceCard.tsx
+++ src/components/_cards/PerformanceCard.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic'
-import { Box, BoxProps, HStack, VStack } from '@chakra-ui/react'
+import { Box, BoxProps, Circle, HStack, Text, VStack } from '@chakra-ui/react'
 import { Serie } from '@nivo/line'
 import { VFC } from 'react'
 import { Card } from './Card'
@@ -51,6 +51,16 @@ export const PerformanceCard: VFC<Props> = props => {
         <Card flex={1} overflow='visible' {...cardProps}>
           <VStack spacing={6} align='stretch' divider={<CardDivider />}>
             <Box h='20rem'>
+              <HStack spacing={6}>
+                <HStack spacing={1}>
+                  <Circle bg='deepSkyBlue.500' size={3} />
+                  <Text fontSize='xs'>Apy</Text>
+                </HStack>
+                <HStack spacing={1}>
+                  <Circle bg='brilliantRose.500' size={3} />
+                  <Text fontSize='xs'>Volume</Text>
+                </HStack>
+              </HStack>
               <LineChart
                 data={data}
                 colors={lineChartTheme}
@@ -70,11 +80,11 @@ export const PerformanceCard: VFC<Props> = props => {
         </Card>
         <Card display='flex' alignItems='center' {...cardProps}>
           <CardStatColumn spacing={6}>
-            <CardStat label='24h cellar apy' labelIcon stat='+3.75%' />
+            <CardStat label='24h cellar apy' labelIcon stat='50%' />
             <CardStat
               label='24h volume'
               labelIcon
-              stat='+12.5K USD'
+              stat='+12.5K USDC'
               statIcon={BsCurrencyDollar}
             />
             <CardStat
