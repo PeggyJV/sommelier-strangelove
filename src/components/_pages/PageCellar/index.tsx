5bf0a373e325adceb920e7eae3319be57c90c863
Xander Low <alexanderjameslow@gmail.com>
2022-03-21 13:07:24 -0700

    refactored: path gen strategy

--- src/components/_pages/PageCellar/index.tsx
+++ src/components/_pages/PageCellar/index.tsx
@@ -21,6 +21,8 @@ import { BreadCrumb } from 'components/BreadCrumb'
 import { Copy } from './Copy'
 import { VFC } from 'react'
 import { BondingTable } from 'components/_tables/BondingTable'
+import { CellarPageProps } from 'pages/cellars/[id]'
+import { useGetCellarQuery } from 'generated/subgraph'
 
 const gridProps: GridProps = {
   gap: 6,
@@ -34,17 +36,26 @@ const investGridProps: GridProps = {
 
 const placeholderButtons = ['1D', '1W', 'All Time']
 
-const PageCellar: VFC = () => {
+const PageCellar: VFC<CellarPageProps> = ({ data: staticData }) => {
   const [auth] = useConnect()
-
   const isConnected = auth.data.connected
+  const { cellar: staticCellar } = staticData
+  const { id, name } = staticCellar!
+  const [cellarResult] = useGetCellarQuery({
+    variables: {
+      cellarAddress: id
+    }
+  })
+  const { data } = cellarResult
+
+  const { cellar } = data || {}
 
   return (
     <Layout>
       <Section>
         <VStack spacing={4} align='stretch'>
           <BreadCrumb fontSize='xl' />
-          <Heading pb={4}>Cellar Presentation Name</Heading>
+          <Heading pb={4}>{name}</Heading>
           <UserPerformanceCard />
         </VStack>
       </Section>
