686e6eda66a3d9f44a8d482eafe7583474995380
Xander Low <alexanderjameslow@gmail.com>
2022-03-29 17:08:07 -0700

    refactored: updated logo

--- src/components/Nav/Brand.tsx
+++ src/components/Nav/Brand.tsx
@@ -1,20 +1,18 @@
 import { HStack, Img, Text } from '@chakra-ui/react'
 import Link from 'components/Link'
+import { LogoTextIcon } from 'components/_icons'
 import React from 'react'
 
 const Brand = () => {
   return (
     <Link px={5} href='/'>
       <HStack>
-        <Img src='/placeholders/logomark.png' boxSize={10} />
-        <Text
-          fontSize='lg'
-          fontWeight='bold'
-          fontFamily='brand'
-          textTransform='uppercase'
-        >
-          Sommelier
-        </Text>
+        <LogoTextIcon
+          w='9rem'
+          h='2rem'
+          color='sunsetOrange'
+          _hover={{ color: 'white' }}
+        />
       </HStack>
     </Link>
   )
