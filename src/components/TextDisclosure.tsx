cbcf6aacfe8361d976354b91a1b8a448dc743904
Xander Low <alexanderjameslow@gmail.com>
2022-04-01 14:59:51 -0700

    added: overlay gradient while hidden

--- src/components/TextDisclosure.tsx
+++ src/components/TextDisclosure.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, Flex, useDisclosure, VStack } from '@chakra-ui/react'
+import { Box, BoxProps, Flex, useDisclosure, useTheme } from '@chakra-ui/react'
 import { BaseButton } from 'components/_buttons/BaseButton'
 import React, { VFC } from 'react'
 import { BiMinus } from 'react-icons/bi'
@@ -6,11 +6,23 @@ import { BsPlus } from 'react-icons/bs'
 
 export const TextDisclosure: VFC<BoxProps> = ({ children, ...rest }) => {
   const { isOpen, onClose, onOpen } = useDisclosure()
+  const theme = useTheme()
 
   return (
     <Flex direction='column' h='100%'>
       <Box flex={1} position='relative' overflow='hidden' {...rest}>
-        <Box position={isOpen ? 'relative' : 'absolute'}>{children}</Box>
+        <Box position={isOpen ? 'relative' : 'absolute'}>
+          <Box
+            w='100%'
+            h='200px'
+            pos='absolute'
+            display={isOpen ? 'none' : 'block'}
+            bottom='190px'
+            bg={`linear-gradient(transparent 60%, ${theme.colors.offBlack} 90%)`}
+            zIndex={2}
+          />
+          {children}
+        </Box>
       </Box>
       <BaseButton
         mt={4}
