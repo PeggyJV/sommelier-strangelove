728f3db204885b22c7165a62be478bd8b3447e8b
Xander Low <alexanderjameslow@gmail.com>
2022-04-01 15:42:38 -0700

    removed: unused code

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -4,8 +4,6 @@ export const styles: Styles = {
   global: {
     'html, body': {
       bgColor: 'backgrounds.offBlack',
-      // bgImage: 'url("/assets/top-left-bg.png")',
-      bgRepeat: 'no-repeat',
       color: 'text.body.light'
     },
     body: {
