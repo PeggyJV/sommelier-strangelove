5b558e0ac8a7d2cd10403e70966b41a8542c9d09
Xander Low <alexanderjameslow@gmail.com>
2022-04-01 15:01:34 -0700

    refactored: default z index

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -3,8 +3,13 @@ import { Styles } from '@chakra-ui/theme-tools'
 export const styles: Styles = {
   global: {
     'html, body': {
-      bg: 'backgrounds.offBlack',
+      bgColor: 'backgrounds.offBlack',
+      // bgImage: 'url("/assets/top-left-bg.png")',
+      bgRepeat: 'no-repeat',
       color: 'text.body.light'
+    },
+    body: {
+      zIndex: 'hide'
     }
   }
 }
