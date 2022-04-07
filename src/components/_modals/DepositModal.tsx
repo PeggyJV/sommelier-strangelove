fa53450d2b46c227c5e79018987639b4662bf716
Xander Low <xander@xandydandy.com>
2022-04-07 14:41:24 -0700

    updated: deposit modal component

--- src/components/_modals/DepositModal.tsx
+++ src/components/_modals/DepositModal.tsx
@@ -1,4 +1,6 @@
-import { Box, ModalProps } from '@chakra-ui/react'
+import { Box, HStack, ModalProps, Text, VStack } from '@chakra-ui/react'
+import { DepositForm } from 'components/_forms/DepositForm'
+import { CardHeading } from 'components/_typography/CardHeading'
 import { VFC } from 'react'
 import BaseModal from './BaseModal'
 
@@ -7,7 +9,27 @@ type DepositModalProps = Pick<ModalProps, 'isOpen' | 'onClose'>
 const DepositModal: VFC<DepositModalProps> = props => {
   return (
     <BaseModal heading='Deposit' {...props}>
-      <Box>DepositModal</Box>
+      <VStack pb={10} spacing={6} align='stretch'>
+        <HStack spacing={6}>
+          <VStack align='flex-start'>
+            <CardHeading>cellar</CardHeading>
+            <Text as='span'>CAave</Text>
+          </VStack>
+          <VStack align='flex-start'>
+            <CardHeading>minimum deposit</CardHeading>
+            <Text as='span'>$10 USDC</Text>
+          </VStack>
+          <VStack align='flex-start'>
+            <CardHeading>deposit clears in</CardHeading>
+            <Text as='span'>6d 4h 23m</Text>
+          </VStack>
+        </HStack>
+        <VStack align='flex-start'>
+          <CardHeading>available</CardHeading>
+          <Text as='span'>10,000 USDC</Text>
+        </VStack>
+      </VStack>
+      <DepositForm />
     </BaseModal>
   )
 }
