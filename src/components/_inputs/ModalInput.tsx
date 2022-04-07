f370247e13256368153771649e663afd8334edd0
Xander Low <xander@xandydandy.com>
2022-04-07 14:40:59 -0700

    added: modal input

---
+++ src/components/_inputs/ModalInput.tsx
@@ -0,0 +1,26 @@
+import { forwardRef, Input, InputProps } from '@chakra-ui/react'
+import { VFC } from 'react'
+
+export const ModalInput: VFC<InputProps> = forwardRef<InputProps, 'input'>(
+  (props, ref) => {
+    return (
+      <Input
+        ref={ref}
+        size='lg'
+        py={7}
+        fontWeight='bold'
+        borderRadius={16}
+        borderColor='warmPink'
+        placeholder='Enter Amount'
+        bg='transparentPurple'
+        _invalid={{
+          borderColor: 'energyYellow'
+        }}
+        _hover={{
+          borderColor: 'warmPink'
+        }}
+        {...props}
+      />
+    )
+  }
+)
