e9ebc994514de0fcb8b3972cbce7ba9e6973030a
Xander Low <xander@xandydandy.com>
2022-04-14 23:32:48 +0100

    refactored: tvm logic

--- src/components/_pages/PageCellar.tsx
+++ src/components/_pages/PageCellar.tsx
@@ -1,10 +1,10 @@
+import { VFC } from 'react'
 import { Heading, HeadingProps, HStack, VStack } from '@chakra-ui/react'
 import Layout from 'components/Layout'
 import { PerformanceCard } from 'components/_cards/PerformanceCard'
 import { Section } from 'components/_layout/Section'
 import { useConnect } from 'wagmi'
 import { PortfolioCard } from 'components/_cards/PortfolioCard'
-import { VFC } from 'react'
 import { CellarPageProps } from 'pages/cellars/[id]'
 import { useGetCellarQuery } from 'generated/subgraph'
 import StrategyBreakdownCard from 'components/_cards/StrategyBreakdownCard'
@@ -33,7 +33,12 @@ const PageCellar: VFC<CellarPageProps> = ({ data: staticData }) => {
   const { data } = cellarResult
   const { cellar } = data || {}
   const { tvlTotal } = cellar || {}
-  const tvm = tvlTotal && (parseInt(tvlTotal) / 1000000).toFixed(2)
+  const tvmVal =
+    tvlTotal &&
+    Intl.NumberFormat('en-US', {
+      notation: 'compact',
+      maximumFractionDigits: 2
+    }).format(parseInt(tvlTotal))
 
   return (
     <Layout>
@@ -45,7 +50,7 @@ const PageCellar: VFC<CellarPageProps> = ({ data: staticData }) => {
             </Link>
             <Heading>C{name}</Heading>
           </HStack>
-          <CellarStats tvm={`$${tvm}M USDC`} apy='8.88' trending='up' />
+          <CellarStats tvm={`$${tvmVal} USDC`} apy='8.88' trending='up' />
         </HStack>
         <VStack spacing={4} align='stretch'>
           <Heading {...h2Styles}>Your Portfolio</Heading>
