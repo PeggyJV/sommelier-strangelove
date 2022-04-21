6c4676791486855f2c0a00a32a5d9599e8825a5f
Ryan Warner <warner.ryan@gmail.com>
2022-04-21 08:27:51 -0400

    remove .vscode from gitignore and add prettier config and auto fix on save

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -1,11 +1,10 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Heading } from "@chakra-ui/react"
 
 export const Education: React.FC = () => {
   return (
     <Box>
       <Heading as="h3" my="h5">
-
-      Education
+        Education
       </Heading>
     </Box>
   )
