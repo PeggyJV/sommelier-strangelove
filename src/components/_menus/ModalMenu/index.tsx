3b9a23016fed68e0ea1cb446c4027b6c71fe8af2
Xander Low <alexanderjameslow@gmail.com>
2022-04-21 15:54:21 -0700

    refactored: moved token config into sibling file

--- src/components/_menus/ModalMenu/index.tsx
+++ src/components/_menus/ModalMenu/index.tsx
@@ -2,8 +2,6 @@ import {
   Box,
   HStack,
   Icon,
-  Image,
-  ImageProps,
   Menu,
   MenuButton,
   MenuItemOption,
@@ -11,36 +9,9 @@ import {
   MenuOptionGroup,
   MenuProps
 } from '@chakra-ui/react'
-import { ReactElement, useState, VFC } from 'react'
+import { useState, VFC } from 'react'
 import { FaChevronDown } from 'react-icons/fa'
-
-interface Token {
-  icon: ReactElement
-  symbol: string
-  address: string
-}
-
-const MenuIcon: VFC<ImageProps> = ({ alt, ...rest }) => (
-  <Image boxSize={4} alt={alt} {...rest} />
-)
-
-const tokenConfig: Token[] = [
-  {
-    icon: <MenuIcon src='/assets/icons/usdt.svg' alt='Tether logo' />,
-    symbol: 'USDT',
-    address: '0xdac17f958d2ee523a2206206994597c13d831ec7'
-  },
-  {
-    icon: <MenuIcon src='/assets/icons/usdc.svg' alt='USD Coin logo' />,
-    symbol: 'USDC',
-    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-  },
-  {
-    icon: <MenuIcon src='/assets/icons/aave.svg' alt='Aave logo' />,
-    symbol: 'AAVE',
-    address: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9'
-  }
-]
+import { Token, tokenConfig } from './tokenConfig'
 
 export const ModalMenu: VFC<MenuProps> = props => {
   const [value, setValue] = useState<Token>()
