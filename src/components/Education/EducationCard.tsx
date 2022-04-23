b80fd483994d9a79b4ccc0db724c1e4e1d8d59de
Ryan Warner <warner.ryan@gmail.com>
2022-04-23 11:01:35 -0400

    add missing font weights and education images

--- src/components/Education/EducationCard.tsx
+++ src/components/Education/EducationCard.tsx
@@ -1,7 +1,9 @@
-import { Flex, Heading, Text } from "@chakra-ui/react"
+import { Flex, Heading, Img, Text } from "@chakra-ui/react"
 import Link from "components/Link"
 import LinkProps from "types/LinkProps"
 import { EduItem } from "./types"
+import { imageStyles } from "./imageStyles"
+import { FaRegIdBadge } from "react-icons/fa"
 
 type Props = EduItem & LinkProps
 
@@ -18,14 +20,30 @@ export const EducationCard: React.FC<Props> = ({
         alignItems="center"
         borderRadius="16px"
         width="320px"
-        height="80px"
+        height="96px"
         border="8px solid rgba(78, 56, 156, 0.08)"
         padding="0 1rem"
+        position="relative"
+        overflow="hidden"
+        boxSizing="border-box"
+        _hover={{
+          backgroundColor: "rgba(78, 56, 156, 0.04)",
+          border: "8px solid rgba(78, 56, 156, 0.12)",
+        }}
       >
-        <Heading as="h3" size="sm">
+        <Img
+          src="assets/images/burst.png"
+          position="absolute"
+          width="160px"
+          zIndex="1"
+          {...imageStyles[variant]}
+        />
+        <Heading as="h3" size="sm" zIndex="2" fontWeight="bold">
           {title}
         </Heading>
-        <Text fontSize="0.75rem">Read more</Text>
+        <Text fontSize="0.75rem" zIndex="2" fontWeight="bold">
+          Read more
+        </Text>
       </Flex>
     </Link>
   )
