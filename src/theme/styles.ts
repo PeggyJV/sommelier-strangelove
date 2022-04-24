4b7396c77ce762eece70624aef4b2e9292d94ab9
Ryan Warner <warner.ryan@gmail.com>
2022-04-24 09:24:56 -0400

    remove blue bg on mobile links

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,13 +1,16 @@
-import { Styles } from '@chakra-ui/theme-tools'
+import { Styles } from "@chakra-ui/theme-tools"
 
 export const styles: Styles = {
   global: {
-    'html, body': {
-      bgColor: 'backgrounds.offBlack',
-      color: 'text.body.light'
+    "html, body": {
+      bgColor: "backgrounds.offBlack",
+      color: "text.body.light",
     },
     body: {
-      zIndex: 'hide'
-    }
-  }
+      zIndex: "hide",
+    },
+    a: {
+      "-webkit-tap-highlight-color": "rgba(0,0,0,0)",
+    },
+  },
 }
