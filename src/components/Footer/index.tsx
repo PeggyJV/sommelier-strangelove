5e40bedda3191a9ce25365f6a36288927b1087ce
Ryan Warner <warner.ryan@gmail.com>
2022-04-24 07:45:41 -0400

    footer style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 import {
   Container,
   ContainerProps,
+  Flex,
   HStack,
   Text,
 } from "@chakra-ui/react"
@@ -11,24 +12,31 @@ import { FooterLink } from "./FooterLink"
 const Footer: VFC<ContainerProps> = (props) => {
   return (
     <Container
+      as="footer"
       display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-      py={10}
-      borderTop="1px solid"
-      borderColor="rgba(203, 198, 209, 0.25)"
       maxW="container.lg"
       {...props}
     >
-      <Text fontSize="xs">
-        &copy; {new Date().getFullYear()} Sommelier
-      </Text>
-      <HStack spacing={4} justify="center" align="center">
-        <FooterLink href="/">Documentation</FooterLink>
-        <FooterLink href="/terms">Terms</FooterLink>
-        <FooterLink href="/privacy">Privacy</FooterLink>
-        <Socials />
-      </HStack>
+      <Flex
+        ml={4}
+        mr={4}
+        borderTop="1px solid"
+        borderColor="rgba(203, 198, 209, 0.25)"
+        justifyContent="space-between"
+        alignItems="center"
+        width="100%"
+        py={10}
+      >
+        <Text fontSize="xs">
+          &copy; {new Date().getFullYear()} Sommelier
+        </Text>
+        <HStack spacing={4} justify="center" align="center">
+          <FooterLink href="/">Documentation</FooterLink>
+          <FooterLink href="/terms">Terms</FooterLink>
+          <FooterLink href="/privacy">Privacy</FooterLink>
+          <Socials />
+        </HStack>
+      </Flex>
     </Container>
   )
 }
