a9a253c7d53d96c6d686b182b56c50eac4e4dbfa
Xander Low <alexanderjameslow@gmail.com>
2022-04-25 14:00:36 -0700

    added: current deposits component

---
+++ src/components/CurrentDeposits.tsx
@@ -0,0 +1,55 @@
+import { VFC } from "react"
+import dynamic from "next/dynamic"
+import { Box, HStack, Text, useTheme, VStack } from "@chakra-ui/react"
+const BarChart = dynamic(
+  () => import("components/_charts/BarChart"),
+  {
+    ssr: false,
+  }
+)
+
+interface CurrentDepositsProps {
+  currentDeposits?: string
+  cellarCap?: string
+}
+
+export const CurrentDeposits: VFC<CurrentDepositsProps> = ({
+  currentDeposits,
+  cellarCap,
+}) => {
+  const theme = useTheme()
+  const dataPresent = currentDeposits && cellarCap
+  const data = dataPresent
+    ? [{ currentDeposits: parseInt(currentDeposits) }]
+    : []
+
+  return (
+    <VStack align="stretch">
+      <HStack justify="space-between" align="flex-end">
+        <Text fontSize="0.625rem" color="text.body.lightMuted">
+          Current Deposits
+        </Text>
+        <Text fontSize="xs">USDC</Text>
+      </HStack>
+      <Box w="100%" h="4px" bg="#252429" overflow="hidden">
+        {/* @ts-ignore */}
+        <BarChart
+          layout="horizontal"
+          colors={theme.colors.lime}
+          keys={["currentDeposits"]}
+          data={data}
+          maxValue={dataPresent ? parseInt(cellarCap) : "auto"}
+          enableLabel={false}
+          enableGridX={false}
+          enableGridY={false}
+        />
+      </Box>
+      <HStack justify="space-between" align="flex-end">
+        <Text fontSize="0.625rem" color="text.body.lightMuted">
+          Cellar Cap
+        </Text>
+        <Text fontSize="xs">USDC</Text>
+      </HStack>
+    </VStack>
+  )
+}
