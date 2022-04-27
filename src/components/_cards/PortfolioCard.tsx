013197ced2318cb5ba483a6efacb9a395e66aedf
Xander Low <alexanderjameslow@gmail.com>
2022-04-27 12:41:06 -0700

    updated: card styles

--- src/components/_cards/PortfolioCard.tsx
+++ src/components/_cards/PortfolioCard.tsx
@@ -19,87 +19,92 @@ import { InlineImage } from "components/InlineImage"
 
 export const PortfolioCard: VFC<BoxProps> = () => {
   return (
-    <Card p={2} bg="backgrounds.glassy">
-      <Card px={10} py={6} bg="backgrounds.black">
-        <CardStatRow
-          spacing={14}
-          align="flex-start"
-          justify="flex-start"
+    <Card
+      px={10}
+      py={6}
+      bg="backgrounds.glassyPurple"
+      borderWidth={8}
+      borderRadius={16}
+      borderColor="backgrounds.glassy"
+    >
+      <CardStatRow
+        spacing={14}
+        align="flex-start"
+        justify="flex-start"
+      >
+        <SimpleGrid
+          templateColumns="repeat(2, max-content)"
+          spacing={4}
         >
-          <SimpleGrid
-            templateColumns="repeat(2, max-content)"
-            spacing={4}
-          >
-            <CardStat label="net value" labelIcon="">
-              $0.00
-            </CardStat>
-            <CardStat label="deposit assets">
-              <HStack spacing={-1.5}>
-                {tokenConfig.reverse().map((token) => {
-                  const { src, alt, address } = token
+          <CardStat label="net value" labelIcon="">
+            $0.00
+          </CardStat>
+          <CardStat label="deposit assets">
+            <HStack spacing={-1.5}>
+              {tokenConfig.reverse().map((token) => {
+                const { src, alt, address } = token
 
-                  return (
-                    <Avatar
-                      key={address}
-                      size="xs"
-                      src={src}
-                      name={alt}
-                      borderWidth={2}
-                      borderColor="black"
-                      bg="black"
-                      _notLast={{
-                        opacity: 0.65,
-                      }}
-                    />
-                  )
-                })}
-              </HStack>
-            </CardStat>
-            <CardStat label="apy" labelIcon="">
-              0.00%
-            </CardStat>
-            <HStack>
-              <DepositButton />
-              <WithdrawButton />
+                return (
+                  <Avatar
+                    key={address}
+                    size="xs"
+                    src={src}
+                    name={alt}
+                    borderWidth={2}
+                    borderColor="black"
+                    bg="black"
+                    _notLast={{
+                      opacity: 0.65,
+                    }}
+                  />
+                )
+              })}
             </HStack>
-          </SimpleGrid>
-          <SimpleGrid
-            templateColumns="repeat(2, max-content)"
-            spacing={4}
-          >
-            <VStack align="flex-start">
-              <CardStat label="available tokens">
-                <InlineImage
-                  src="/assets/icons/aave.svg"
-                  alt="aave logo"
-                />
-                0
-              </CardStat>
-              <BondButton />
-            </VStack>
-            <VStack align="flex-start">
-              <CardStat label="bonded tokens">
-                <InlineImage
-                  src="/assets/icons/aave.svg"
-                  alt="aave logo"
-                />
-                0
-              </CardStat>
-              <UnbondButton />
-            </VStack>
-          </SimpleGrid>
+          </CardStat>
+          <CardStat label="apy" labelIcon="">
+            0.00%
+          </CardStat>
+          <HStack>
+            <DepositButton />
+            <WithdrawButton />
+          </HStack>
+        </SimpleGrid>
+        <SimpleGrid
+          templateColumns="repeat(2, max-content)"
+          spacing={4}
+        >
           <VStack align="flex-start">
-            <CardStat label="rewards">
+            <CardStat label="available tokens">
               <InlineImage
-                src="/assets/icons/somm.svg"
+                src="/assets/icons/aave.svg"
                 alt="aave logo"
               />
               0
             </CardStat>
-            <ClaimButton />
+            <BondButton />
           </VStack>
-        </CardStatRow>
-      </Card>
+          <VStack align="flex-start">
+            <CardStat label="bonded tokens">
+              <InlineImage
+                src="/assets/icons/aave.svg"
+                alt="aave logo"
+              />
+              0
+            </CardStat>
+            <UnbondButton />
+          </VStack>
+        </SimpleGrid>
+        <VStack align="flex-start">
+          <CardStat label="rewards">
+            <InlineImage
+              src="/assets/icons/somm.svg"
+              alt="aave logo"
+            />
+            0
+          </CardStat>
+          <ClaimButton />
+        </VStack>
+      </CardStatRow>
     </Card>
   )
 }
