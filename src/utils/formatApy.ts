e1f55b82942076a86513d7067d6e11ff9acfc710
Xander Low <xander@xandydandy.com>
2022-05-04 12:12:08 -0700

    chore: fix logic

--- src/utils/formatApy.ts
+++ src/utils/formatApy.ts
@@ -1,25 +1,40 @@
 import { CellarDayData } from "generated/subgraph"
 
-export const formatApy = (dayDatas?: CellarDayData[]) => {
+export const formatApy = (
+  dayDatas?: Partial<CellarDayData>[]
+): string | undefined => {
   if (!dayDatas) {
-    return null
+    return undefined
   }
 
-  const totalEarnings = dayDatas.reduce(
-    (acc, dayData) => acc + dayData.earnings,
+  const totalEarnings: number = dayDatas.reduce((acc, dayData, i) => {
+    const previousDayData = i > 0 ? dayDatas[i - 1] : undefined
+    const rebalance: boolean = dayData.date === previousDayData?.date
+    const rebalanceEarnings: number =
+      dayData.earnings + previousDayData?.earnings
 
-    {} as number
-  )
-  const totalTvlInvested = dayDatas.reduce(
-    (acc, dayData) => acc + dayData.tvlInvested,
-    {} as number
-  )
-  const totalYield = totalEarnings / totalTvlInvested
-  const days = dayDatas.length
-  const periodsInYear = 365 / days
-  const apy = ((1 + totalYield) ^ (periodsInYear - 1)) * 100
+    return rebalance
+      ? acc + rebalanceEarnings
+      : acc + dayData.earnings
+  }, 0)
+  const tvlInvested: number =
+    dayDatas[dayDatas.length - 1].tvlInvested
+  const totalYield: number = totalEarnings / tvlInvested
+  const days: number = dayDatas.length
+  const periodsInYear: number = 365 / days
+  const apy: number = ((1 + totalYield) ^ (periodsInYear - 1)) * 100
 
-  const apyVal = (apy * 100).toFixed(2)
+  const apyVal: string = (apy * 100).toFixed(2)
+
+  console.log({
+    totalEarnings,
+    tvlInvested,
+    totalYield,
+    days,
+    periodsInYear,
+    apy,
+    apyVal,
+  })
 
   return apyVal
 }
