73bafccff800b6f00784dc22121cdb25ae458f96
Xander Low <alexanderjameslow@gmail.com>
2022-05-16 13:28:13 -0700

    merged staging and commented out unused code

--- src/components/_cards/BondingTableCard.tsx
+++ src/components/_cards/BondingTableCard.tsx
@@ -19,38 +19,39 @@ import { InlineImage } from "components/InlineImage"
 import { InformationIcon } from "components/_icons"
 import { useAaveStaker } from "context/aaveStakerContext"
 import { toEther } from "utils/formatCurrency"
+
 interface BondingTableCardProps extends TableProps {
   data?: any
 }
 
-interface BondingPeriod {
-  bondingPeriod: "7 Days" | "14 Days" | "21 Days"
-  amount: number
-  value: 1.1 | 1.25 | 1.5
-  checked?: boolean
-  canUnbond: boolean
-}
+// interface BondingPeriod {
+//   bondingPeriod: "7 Days" | "14 Days" | "21 Days"
+//   amount: number
+//   value: 1.1 | 1.25 | 1.5
+//   checked?: boolean
+//   canUnbond: boolean
+// }
 
-const placeholderData: BondingPeriod[] = [
-  {
-    bondingPeriod: "14 Days",
-    amount: 20.01,
-    value: 1.25,
-    canUnbond: false,
-  },
-  {
-    bondingPeriod: "7 Days",
-    amount: 63.99,
-    value: 1.1,
-    canUnbond: true,
-  },
-  {
-    bondingPeriod: "21 Days",
-    amount: 40,
-    value: 1.5,
-    canUnbond: false,
-  },
-]
+// const placeholderData: BondingPeriod[] = [
+//   {
+//     bondingPeriod: "14 Days",
+//     amount: 20.01,
+//     value: 1.25,
+//     canUnbond: false,
+//   },
+//   {
+//     bondingPeriod: "7 Days",
+//     amount: 63.99,
+//     value: 1.1,
+//     canUnbond: true,
+//   },
+//   {
+//     bondingPeriod: "21 Days",
+//     amount: 40,
+//     value: 1.5,
+//     canUnbond: false,
+//   },
+// ]
 
 const BondingTableCard: VFC<BondingTableCardProps> = ({
   data,
@@ -59,6 +60,7 @@ const BondingTableCard: VFC<BondingTableCardProps> = ({
   const { userStakeData } = useAaveStaker()
   const { userStakes } = userStakeData
   console.log(userStakes)
+
   return (
     <TransparentCard>
       <TableContainer>
@@ -168,11 +170,11 @@ const BondingTableCard: VFC<BondingTableCardProps> = ({
                     </Td> */}
                     <SecondaryButton
                       size="sm"
-                      onClick={() =>
-                        window.alert(
-                          `You've bonded for ${bondingPeriod}. You earned at a rate of ${value}x.`
-                        )
-                      }
+                      // onClick={() =>
+                      //   window.alert(
+                      //     `You've bonded for ${bondingPeriod}. You earned at a rate of ${value}x.`
+                      //   )
+                      // }
                     >
                       Unbond
                     </SecondaryButton>
