bf41b1e22d6f4b5e017af8c61b61df0a14f640c6
Xander Low <alexanderjameslow@gmail.com>
2022-05-17 14:25:18 -0700

    added: bg blur

--- src/components/_cards/TransparentCard.tsx
+++ src/components/_cards/TransparentCard.tsx
@@ -9,6 +9,7 @@ const TransparentCard: VFC<BoxProps> = (props) => {
       borderWidth={1}
       borderRadius={16}
       borderColor="surface.secondary"
+      backdropFilter="blur(8px)"
       {...props}
     />
   )
