200b9e0521143139a4f962b900c1defda3f3a681
Xander Low <alexanderjameslow@gmail.com>
2022-05-18 16:27:48 -0700

    fixed: peculiar resizing bug with performance split chart

--- src/components/_cards/CellarDetailsCard.tsx
+++ src/components/_cards/CellarDetailsCard.tsx
@@ -68,7 +68,7 @@ const CellarDetailsCard: VFC<CellarDetailsProps> = ({
           align="flex-start"
           direction={{ sm: "column", md: "row" }}
           rowGap={{ sm: 0, md: 4 }}
-          wrap="wrap"
+          wrap={{ sm: "wrap", lg: "nowrap" }}
           divider={
             <CardDivider
               css={{
@@ -133,7 +133,7 @@ const CellarDetailsCard: VFC<CellarDetailsProps> = ({
               </Tooltip>
               <InformationIcon color="neutral.300" boxSize={3} />
             </HStack>
-            <Box h="4px">
+            <Box h="4px" maxW={{ lg: 318 }}>
               {/* @ts-ignore */}
               <BarChart
                 layout="horizontal"
