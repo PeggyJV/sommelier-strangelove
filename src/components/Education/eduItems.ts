706e7604e81d42c607e5baffdb91c55eeb386fb2
Ryan Warner <warner.ryan@gmail.com>
2022-05-20 15:23:07 -0400

    update notion links on home page

--- src/components/Education/eduItems.ts
+++ src/components/Education/eduItems.ts
@@ -3,17 +3,17 @@ import { EduCardVariant, EduItem } from "./types"
 export const eduItems: EduItem[] = [
   {
     title: "What are Cellars?",
-    url: "https://notion.so",
+    url: "https://tricky-sand-5e6.notion.site/Cellars-fd76e41e2fb4444d87916532dcc928ed",
     variant: EduCardVariant.Left,
   },
   {
     title: "How to earn",
-    url: "https://notion.so",
+    url: "https://tricky-sand-5e6.notion.site/How-to-Earn-with-Sommelier-f53a749b11e844e0934ed3c0982cb3d4",
     variant: EduCardVariant.Top,
   },
   {
     title: "Glossary",
-    url: "https://notion.so",
+    url: "https://tricky-sand-5e6.notion.site/Sommelier-Glossary-32fd1a919c35458bb4c0067a648e9057",
     variant: EduCardVariant.Bottom,
   },
 ]
