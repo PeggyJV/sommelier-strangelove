ce0a2a7c644d83f82f22ef3ecf0f6203b5a4b04e
Elliot Shiu <sandbochs@users.noreply.github.com>
2022-07-08 10:38:59 -0700

    use curl

--- get-maxmind.sh
+++ get-maxmind.sh
@@ -4,7 +4,7 @@ then
   echo "Missing Maxmind License Key, please set it via export MAXMIND_KEY=<your license key>"
 else
   echo "Downloading the maxmind database"
-  wget "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_KEY&suffix=tar.gz" -O maxmind.tar.gz
+  curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_KEY&suffix=tar.gz" --output maxmind.tar.gz
   mkdir -p maxmind
   tar -xf maxmind.tar.gz -C ./maxmind --strip 1
 fi
