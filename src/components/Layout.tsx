af21ea066c5ccbded285dda6ec1bd44230a257f8
Xander Low <alexanderjameslow@gmail.com>
2022-07-19 10:59:46 -0700

    added: geo banner

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,7 @@ import { Container, Flex, FlexProps } from "@chakra-ui/react"
 import Footer from "./Footer"
 import { BackgroundAssets } from "./BackgroundAssets"
 import { useGeo } from "context/geoContext"
+import { GeoBanner } from "./_banners/GeoBanner"
 
 export const Layout: VFC<FlexProps> = ({ children, ...rest }) => {
   const { isRestricted } = useGeo() || {}
@@ -14,7 +15,7 @@ export const Layout: VFC<FlexProps> = ({ children, ...rest }) => {
       <Flex minH="100vh" flexDir="column" {...rest}>
         <Nav />
         <Container as="main" flex={1} pt={40} maxW="container.lg">
-          {isRestricted && <Flex>NO THX</Flex>}
+          {isRestricted && <GeoBanner />}
           {children}
         </Container>
         <Footer />
