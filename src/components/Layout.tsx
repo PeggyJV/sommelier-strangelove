56517bd116f669ef01127679b18028f72db5f607
Xander Low <alexanderjameslow@gmail.com>
2022-07-18 18:02:01 -0700

    chore: complete build-out of warning banner and UI booleans"

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,14 +3,18 @@ import { Nav } from "./Nav"
 import { Container, Flex, FlexProps } from "@chakra-ui/react"
 import Footer from "./Footer"
 import { BackgroundAssets } from "./BackgroundAssets"
+import { useCheckIP } from "context/checkIPContext"
 
 export const Layout: VFC<FlexProps> = ({ children, ...rest }) => {
+  const { isRestricted } = useCheckIP() || {}
+
   return (
     <>
       <BackgroundAssets />
       <Flex minH="100vh" flexDir="column" {...rest}>
         <Nav />
         <Container as="main" flex={1} pt={40} maxW="container.lg">
+          {isRestricted && <Flex>NO THX</Flex>}
           {children}
         </Container>
         <Footer />
