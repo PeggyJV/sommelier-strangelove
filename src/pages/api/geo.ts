35d4a25b19c856a1fa3f3b77b800bdbae64ccc95
Xander Low <alexanderjameslow@gmail.com>
2022-07-20 16:34:33 -0700

    updated headers

--- src/pages/api/geo.ts
+++ src/pages/api/geo.ts
@@ -56,7 +56,17 @@ const testGeo = (req: NextApiRequest, res: NextApiResponse) => {
     return false
   }
 
+  // @ts-ignore boolean missing from setHeader types
+  res.setHeader("Access-Control-Allow-Credentials", true)
   res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  )
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  )
 
   res.status(200).json({
     country,
