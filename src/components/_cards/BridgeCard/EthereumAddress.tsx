f58623abb4f5284325d8058515084b4d279c0e98
Nur Fikri <codingki@gmail.com>
2022-08-15 13:00:30 +0700

    feat: add account logic on bridge page

--- src/components/_cards/BridgeCard/EthereumAddress.tsx
+++ src/components/_cards/BridgeCard/EthereumAddress.tsx
@@ -1,27 +1,32 @@
 import { Stack, Flex, Text } from "@chakra-ui/react"
 import React from "react"
+import { useAccount } from "wagmi"
 
-export const EthereumAddress: React.FC = () => (
-  <Stack spacing={2}>
-    <Text fontWeight="bold" color="neutral.400" fontSize="xs">
-      Ethereum Address
-    </Text>
-    <Flex
-      backgroundColor="surface.tertiary"
-      justifyContent="space-between"
-      borderRadius={16}
-      px={4}
-      height="64px"
-      alignItems="center"
-    >
-      <Text
-        fontSize="xs"
-        fontWeight="semibold"
-        color="neutral.300"
-        overflow="auto"
-      >
-        0x91BcAACf3A997E467c8A16Fb8C56a80413d0C207
+export const EthereumAddress: React.FC = () => {
+  const [account] = useAccount()
+
+  return (
+    <Stack spacing={2}>
+      <Text fontWeight="bold" color="neutral.400" fontSize="xs">
+        Ethereum Address
       </Text>
-    </Flex>
-  </Stack>
-)
+      <Flex
+        backgroundColor="surface.tertiary"
+        justifyContent="space-between"
+        borderRadius={16}
+        px={4}
+        height="64px"
+        alignItems="center"
+      >
+        <Text
+          fontSize="xs"
+          fontWeight="semibold"
+          color="neutral.300"
+          overflow="auto"
+        >
+          {account.data?.address || "--"}
+        </Text>
+      </Flex>
+    </Stack>
+  )
+}
