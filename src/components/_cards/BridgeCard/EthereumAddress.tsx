c513d1716bcc1db5eef204e601b1660309120626
Nur Fikri <codingki@gmail.com>
2022-08-15 23:19:55 +0700

    feat(ui): add loading state on ethereum address

--- src/components/_cards/BridgeCard/EthereumAddress.tsx
+++ src/components/_cards/BridgeCard/EthereumAddress.tsx
@@ -1,10 +1,9 @@
-import { Stack, Flex, Text } from "@chakra-ui/react"
+import { Stack, Flex, Text, Spinner } from "@chakra-ui/react"
 import React from "react"
 import { useAccount } from "wagmi"
 
 export const EthereumAddress: React.FC = () => {
   const [account] = useAccount()
-
   return (
     <Stack spacing={2}>
       <Text fontWeight="bold" color="neutral.400" fontSize="xs">
@@ -12,20 +11,24 @@ export const EthereumAddress: React.FC = () => {
       </Text>
       <Flex
         backgroundColor="surface.tertiary"
-        justifyContent="space-between"
+        justifyContent="center"
         borderRadius={16}
         px={4}
         height="64px"
         alignItems="center"
       >
-        <Text
-          fontSize="xs"
-          fontWeight="semibold"
-          color="neutral.300"
-          overflow="auto"
-        >
-          {account.data?.address || "--"}
-        </Text>
+        {account.loading ? (
+          <Spinner size="xs" />
+        ) : (
+          <Text
+            fontSize="xs"
+            fontWeight="semibold"
+            color="neutral.300"
+            overflow="auto"
+          >
+            {account.data?.address || "--"}
+          </Text>
+        )}
       </Flex>
     </Stack>
   )
