c86809cd4a074079a9d6a0af45b41826da990e3e
Nur Fikri <codingki@gmail.com>
2022-09-09 07:30:18 +0700

    chore: hooks for handling actions and specific contract branching

---
+++ src/composite-data/hooks/staker/useStakerData.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query"
+import { SommStaking } from "src/abi/types"
+import { fetchStakerData } from "src/composite-data/actions/staker/AAVE_STAKER/fetchStakerData"
+import { ContractProps } from "../types"
+
+export const useStakerData = ({
+  staker,
+}: {
+  staker: Omit<ContractProps, "signer">
+}) => {
+  const queryStaker = useQuery(["USE_STAKER_DATA"], async () => {
+    if (staker.key === "AAVE_STAKER") {
+      return await fetchStakerData(staker.contract as SommStaking)
+    }
+    throw new Error("UNKNOWN CONTRACT")
+  })
+
+  return {
+    data: queryStaker.data,
+    error: queryStaker.error,
+    isFetching: queryStaker.isFetching,
+    isLoading: queryStaker.isLoading,
+    isRefetching: queryStaker.isRefetching,
+    isSuccess: queryStaker.isSuccess,
+    refetch: queryStaker.refetch,
+    status: queryStaker.status,
+  }
+}
