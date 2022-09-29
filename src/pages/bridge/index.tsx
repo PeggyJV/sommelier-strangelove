abe5935ad8412b049ba3aed92aac7392f4f16cb5
Nur Fikri <codingki@gmail.com>
2022-09-30 01:21:34 +0700

    feat: feature flag for bridge page

--- src/pages/bridge/index.tsx
+++ src/pages/bridge/index.tsx
@@ -1,8 +1,10 @@
+import { Page404 } from "components/_pages/Page404"
 import { PageBridge } from "components/_pages/PageBridge"
 import type { NextPage } from "next"
+import { BRIDGE_PAGE_ENABLED } from "utils/constants"
 
 const Bridge: NextPage = () => {
-  return <PageBridge />
+  return BRIDGE_PAGE_ENABLED ? <PageBridge /> : <Page404 />
 }
 
 export default Bridge
