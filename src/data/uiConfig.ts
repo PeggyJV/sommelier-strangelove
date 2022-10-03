63cca168873f31844e3c0dc1d90d57ccb1af2393
Nur Fikri <codingki@gmail.com>
2022-10-04 04:14:39 +0700

    chore: add ui flag helper functions

---
+++ src/data/uiConfig.ts
@@ -0,0 +1,13 @@
+import { CellarKey, ConfigProps } from "./types"
+
+export const isBondingEnabled = (config: ConfigProps) => {
+  return config.cellar.key === CellarKey.AAVE_V2_STABLE_CELLAR
+}
+
+export const isRewardsEnabled = (config: ConfigProps) => {
+  return config.cellar.key === CellarKey.AAVE_V2_STABLE_CELLAR
+}
+
+export const isCurrentDepositsEnabled = (config: ConfigProps) => {
+  return config.cellar.key === CellarKey.AAVE_V2_STABLE_CELLAR
+}
