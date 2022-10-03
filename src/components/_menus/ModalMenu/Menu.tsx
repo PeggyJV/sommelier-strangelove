3ad14c9e1ab3f41c0dff3da142239012e28faa64
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-10-04 03:17:50 +0700

    remove: 50k deposit limit

--- src/components/_menus/ModalMenu/Menu.tsx
+++ src/components/_menus/ModalMenu/Menu.tsx
@@ -195,26 +195,6 @@ export const Menu: VFC<MenuProps> = ({
                     ) || "Insufficient balance"
                 )
               },
-              depositLessThanFifty: (v) =>
-                v <= 50000 ||
-                "You cannot exceed the cellar limit of $50,000.",
-              depositLimit: (v) => {
-                if (!depositData?.wallet) return true
-
-                const currentDeposits = parseFloat(
-                  toEther(
-                    depositData?.wallet?.currentDeposits!,
-                    18,
-                    false
-                  )
-                )
-                const sum = v + currentDeposits
-
-                return (
-                  sum <= 50000 ||
-                  `You cannot exceed the cellar limit of $50,000. You currently have $${currentDeposits} deposited in this cellar.`
-                )
-              },
             },
           })}
         />
