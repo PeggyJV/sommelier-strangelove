c0cc053b5e19517150252a7cb24ecc8b197f2c8e
Nur Fikri <codingki@gmail.com>
2022-10-14 02:35:06 +0700

    feat: branching lp token tooltip to ui config

--- src/data/uiConfig.ts
+++ src/data/uiConfig.ts
@@ -15,3 +15,11 @@ export const isCurrentDepositsEnabled = (config: ConfigProps) => {
 export const isActiveTokenStrategyEnabled = (config: ConfigProps) => {
   return config.cellar.key === CellarKey.AAVE_V2_STABLE_CELLAR
 }
+
+export const lpTokenTooltipContent = (config: ConfigProps) => {
+  if (config.cellar.key === CellarKey.AAVE_V2_STABLE_CELLAR)
+    return "Unbonded LP tokens earn interest from strategy but do not earn Liquidity Mining rewards"
+  if (config.cellar.key === CellarKey.CLEAR_GATE_CELLAR)
+    return "The LP tokens represent a user's share of the pool and can always be redeemed for the original tokens"
+  return ""
+}
