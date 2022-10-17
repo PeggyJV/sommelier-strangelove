da3cfd74f046ce9afb3c7db67d665fbdab73f029
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-10-18 02:51:37 +0700

    feat: get sanity faq data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
-import PageHome from 'components/_pages/PageHome'
-import type { NextPage } from 'next'
+import PageHome from "components/_pages/PageHome"
+import type { GetStaticProps, NextPage } from "next"
+import { sanityClient } from "src/lib/sanity/client"
+import { sanityFaqQuery } from "src/lib/sanity/queries"
+import { CustomFaqSection } from "types/sanity"
 
-const Home: NextPage = () => {
-  return <PageHome />
+export interface HomeProps {
+  faqData: CustomFaqSection
+}
+
+const Home: NextPage<HomeProps> = ({ faqData }) => {
+  return <PageHome faqData={faqData} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const faqData = await sanityClient.fetch(sanityFaqQuery)
+
+  return {
+    props: {
+      faqData,
+    },
+  }
 }
 
 export default Home
