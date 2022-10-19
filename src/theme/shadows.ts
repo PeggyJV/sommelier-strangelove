f0a5c908df47016d50770a4db96ef7fcd5b95cae
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-10-20 05:52:17 +0700

    added: new shadow color

--- src/theme/shadows.ts
+++ src/theme/shadows.ts
@@ -4,4 +4,5 @@ export const shadows = {
   outline: `0 0 0 3px ${colors.purple.base}`,
   orangeOutline1: `0 0 0 1px ${colors.orange.base}`,
   purpleOutline1: `0 0 0 1px ${colors.purple.base}`,
+  redOutline1: `0 0 0 1px ${colors.red.normal}`,
 }
