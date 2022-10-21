0f25452c56e922648dca34422c392e533fb1b30d
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-10-22 01:04:05 +0700

    created: cellars section

---
+++ src/components/Cellars/StrategyProviders.tsx
@@ -0,0 +1,36 @@
+import { Box, BoxProps, Grid, Heading } from "@chakra-ui/react"
+import { Link } from "components/Link"
+import { SevenSeasIcon } from "components/_icons/SevenSeasIcon"
+import { ClearGateIcon } from "components/_icons/ClearGateIcon"
+import { BecomeProvider } from "./BecomeProvider"
+import { CardBase } from "./CardBase"
+
+export const StrategyProviders: React.FC<BoxProps> = (props) => {
+  return (
+    <Box position="relative" zIndex="2" w="full" {...props}>
+      <Heading as="h3" size="sm" mb={10}>
+        Strategy Providers
+      </Heading>
+      <Grid
+        gap={6}
+        templateColumns={{
+          base: "repeat(1, 1fr)",
+          md: "repeat(2, 1fr)",
+          xl: "repeat(3, 1fr)",
+        }}
+      >
+        <Link isExternal href="https://7seas.capital/">
+          <CardBase justifyContent="center" h="full">
+            <SevenSeasIcon w={{ base: 200, lg: 243 }} h={100} />
+          </CardBase>
+        </Link>
+        <Link isExternal href="https://cleargate.capital/">
+          <CardBase justifyContent="center" h="full">
+            <ClearGateIcon w={{ base: 200, lg: 243 }} h={100} />
+          </CardBase>
+        </Link>
+        <BecomeProvider />
+      </Grid>
+    </Box>
+  )
+}
