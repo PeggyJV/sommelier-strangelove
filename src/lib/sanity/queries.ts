f338b622328d94c89b043664fa5eb195b57e5c14
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-10-21 22:11:53 +0700

    added: sanity home query

--- src/lib/sanity/queries.ts
+++ src/lib/sanity/queries.ts
@@ -1,5 +1,46 @@
 import { groq } from "next-sanity"
 
+export const sanityHomeQuery = groq`
+*[_type == 'home']{
+  ...,
+  sectionCellars{
+    title,
+    subtitle,
+    "protocols": protocols[]->{
+      ...,
+      name,
+      status,
+      "image": image.asset->{
+        metadata,
+        url,
+      },
+    },
+    "stableCoins": stableCoins[]->{
+      ...,
+      name,
+      "image": image.asset->{
+        metadata,
+        url,
+      },
+    },
+  },
+  sectionStrategies{
+    title,
+    subtitle,
+    "strategies": strategies[]->{
+      ...,
+      "stableCoins": stableCoins[]->{
+        ...,
+        name,
+        "image": image.asset->{
+          metadata,
+          url,
+        },
+      },
+    }
+  },
+}[0]`
+
 export const sanityFaqQuery = groq`
 *[_type == 'faqSection']{
   ...,
