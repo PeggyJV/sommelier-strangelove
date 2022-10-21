f44aa0912dbb157e28cc44e892d159d7df5d1838
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-10-21 22:12:35 +0700

    updated: cellars page to get marketing home data

--- src/pages/cellars/[id].tsx
+++ src/pages/cellars/[id].tsx
@@ -3,14 +3,26 @@ import { GetStaticPaths, GetStaticProps, NextPage } from "next"
 
 import { ParsedUrlQuery } from "querystring"
 import { cellarDataMap } from "data/cellarDataMap"
+import { sanityClient } from "src/lib/sanity/client"
+import {
+  sanityFaqQuery,
+  sanityHomeQuery,
+} from "src/lib/sanity/queries"
+import { CustomFaqSection, HomeWithImages } from "types/sanity"
 export interface CellarPageProps {
   id: string
+  faqData: CustomFaqSection
+  data: HomeWithImages
 }
 
 type Params = ParsedUrlQuery & { id: string }
 
-const CellarPage: NextPage<CellarPageProps> = ({ id }) => {
-  return <PageCellar id={id} />
+const CellarPage: NextPage<CellarPageProps> = ({
+  id,
+  faqData,
+  data,
+}) => {
+  return <PageCellar id={id} faqData={faqData} data={data} />
 }
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
@@ -29,10 +41,12 @@ export const getStaticPaths: GetStaticPaths<Params> = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const faqData = await sanityClient.fetch(sanityFaqQuery)
+  const home = await sanityClient.fetch(sanityHomeQuery)
   const { id } = params || {}
   // query subgraph for cellar data of given ID
 
-  return { props: { id } }
+  return { props: { id, faqData, data: home } }
 }
 
 export default CellarPage
