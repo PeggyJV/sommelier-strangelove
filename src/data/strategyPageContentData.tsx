ecda7ecbd26b7e35f584086c853738bf63b42bbb
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-10-25 03:54:25 +0700

    feat: updated strategy page content data

--- src/data/strategyPageContentData.tsx
+++ src/data/strategyPageContentData.tsx
@@ -30,6 +30,11 @@ export const strategyPageContentData = {
           src="/assets/icons/btc.png"
           boxSize={8}
         />
+        <Image
+          alt="eth btc trend"
+          src="/assets/icons/usdc.png"
+          boxSize={8}
+        />
       </>
     ),
     alternativeTo: "Holding ETH or BTC",
@@ -46,6 +51,25 @@ export const strategyPageContentData = {
     howItWorks:
       "The strategy accumulates BTC and ETH relative to USDC with a medium to long-term perspective. The rebalancing decision applies the concept of trend following while also considering the correlation between the portfolio assets and emerging trends. The strategy aims to outperform the benchmarks and have lower volatility and risk than holding BTC or ETH individually or an equally weighted portfolio of BTC and ETH.",
     backtestingImage: "/assets/images/btc-eth-trend-backtesting.png",
+    backtestingText: `Notes: Performance of the strategy from Jan 2019-Jun 2022. Black line for benchmark
+
+      Backtest results:
+      Period: Jan 2019 – June 2022
+      APY: 123.72%
+      Sharpe ratio: 2.117
+      Profit-Loss Ratio: 0.94
+      Worst drawdown: 28.50%
+      Annual Std: 0.431
+
+      Backtest results:
+      Alpha: 0.868
+      Cumulative profit: 1576.59%
+      Win rate: 87%
+      Loss rate: 13%
+      Best month: 52.51%
+      Worst month: -14.21%
+
+      Presented results are based on historical backtests. Past performance is not indicative of future results. Actual performance will depend on market conditions.`,
   },
   [config.CONTRACT.ETH_BTC_MOMENTUM_CELLAR.ADDRESS]: {
     name: "ETH-BTC Momentum Strategy",
@@ -57,7 +81,7 @@ export const strategyPageContentData = {
       <>
         <Image
           alt="eth btc trend"
-          src="/assets/icons/eth-btc-trend.svg"
+          src="/assets/icons/eth-btc-mom.svg"
           boxSize={8}
         />
         <Text>ETHBTCMom</Text>
@@ -75,6 +99,11 @@ export const strategyPageContentData = {
           src="/assets/icons/btc.png"
           boxSize={8}
         />
+        <Image
+          alt="eth btc trend"
+          src="/assets/icons/usdc.png"
+          boxSize={8}
+        />
       </>
     ),
     alternativeTo: "Holding ETH or BTC",
@@ -86,11 +115,30 @@ export const strategyPageContentData = {
         "Uses risk management rules to reduce risks in an unfavorable market.",
       ],
     },
-    howItWorks: `The cellar accumulates BTC and ETH relative to USDC with a medium to long-term perspective. The rebalancing decision is based on price momentum and the asset with higher momentum is assigned to be overweight. The strategy aims to outperform the benchmarks and have lower volatility and risk than holding BTC or ETH individually or in an equally weighted portfolio of BTC and ETH. 
+    howItWorks: `The cellar accumulates BTC and ETH relative to USDC with a medium to long-term perspective. The rebalancing decision is based on price momentum and the asset with higher momentum is assigned to be overweight. The strategy aims to outperform the benchmarks and have lower volatility and risk than holding BTC or ETH individually or in an equally weighted portfolio of BTC and ETH.
     <br/><br/>
     The strategy is long only, but it reduces risk exposure if price momentum is negative. Backtests indicate the cellar will outperform at times when prices are rising moderately or one of the portfolio assets is appreciating faster than the other. The strategy is likely to underperform when there are extreme price appreciations but is expected to capture the majority of any positive price movements. The strategy is expected to outperform in a bear market since it can cut risk exposure, but it is exposed to negative price movements because it is long only and always holds at least a small proportion of risky assets. The benefits of the strategy should emerge within 3-6 month (i.e., medium term) holding period in case of diverse market conditions and are highly likely to emerge for holding periods over 1 year (i.e., long term).
     <br/><br/>
     The strategy is more sensitive to positive market movements than a trend-following strategy and is expected to provide slightly higher returns in a bull market than a similar trend-following strategy but lower returns in a bear market.`,
     backtestingImage: "/assets/images/btc-eth-moment-backtesting.png",
+    backtestingText: `Notes: Performance of the strategy from Jan 2019-Jun 2022. Black line for benchmark
+
+      Backtest results:
+      Period: Jan 2019 – June 2022
+      APY: 84.15%
+      Sharpe ratio: 1.402
+      Profit-Loss Ratio: 1.76
+      Worst drawdown: 60.30%
+      Annual Std: 0.524
+
+      Backtest results:
+      Alpha: 0.671
+      Cumulative profit: 748.12%
+      Win rate: 62%
+      Loss rate: 38%
+      Best month: 51.97%
+      Worst month: -27.50%
+
+      Presented results are based on historical backtests. Past performance is not indicative of future results. Actual performance will depend on market conditions.`,
   },
 }
