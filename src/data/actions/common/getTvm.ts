9d4364eba2276cd3c1016fd5c7b76a799ffe05d7
Nur Fikri <codingki@gmail.com>
2022-10-27 17:37:31 +0700

    feat(ui): remove symbol

--- src/data/actions/common/getTvm.ts
+++ src/data/actions/common/getTvm.ts
@@ -1,22 +1,18 @@
 import { AaveV2CellarV2, ClearGateCellar } from "src/abi/types"
 import { getCalulatedTvl } from "utils/bigNumber"
 import { formatCurrency } from "utils/formatCurrency"
-import { getActiveAsset } from "./getActiveAsset"
 
 export const getTvm = async (
   cellarContract: AaveV2CellarV2 | ClearGateCellar,
   tvlTotal?: string
 ) => {
   try {
-    const activeAsset = await getActiveAsset(cellarContract)
-    const activeSymbol = activeAsset?.symbol
-
     const calculatedTvl = tvlTotal && getCalulatedTvl(tvlTotal, 18)
     const tvmVal = formatCurrency(calculatedTvl)
 
     return {
       value: tvmVal,
-      formatted: `$${tvmVal} ${activeSymbol}`,
+      formatted: `$${tvmVal}`,
     }
   } catch (error) {
     console.warn("Cannot read cellar data", error)
