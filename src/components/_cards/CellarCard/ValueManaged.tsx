eaf1fc6e0bcc1aa4981c6a68c9f7f6c69bb0a888
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-11-03 01:25:27 +0700

    feat(refactor): value managed for new design

--- src/components/_cards/CellarCard/ValueManaged.tsx
+++ src/components/_cards/CellarCard/ValueManaged.tsx
@@ -6,6 +6,7 @@ import {
   Tooltip,
   HStack,
   Spinner,
+  Icon,
 } from "@chakra-ui/react"
 import { CurrentDeposits } from "components/CurrentDeposits"
 import { Label } from "./Label"
@@ -17,6 +18,9 @@ import { useCellarCap } from "data/hooks/useCellarCap"
 import { useCurrentDeposits } from "data/hooks/useCurrentDeposits"
 import { useActiveAsset } from "data/hooks/useActiveAsset"
 import { isCurrentDepositsEnabled } from "data/uiConfig"
+import { useTokenPrice } from "data/hooks/useTokenPrice"
+import { useWeekChange } from "data/hooks/useWeekChange"
+import { FaArrowDown, FaArrowUp } from "react-icons/fa"
 
 interface Props extends BoxProps {
   cellarId: string
@@ -26,6 +30,7 @@ export const ValueManaged: React.FC<Props> = ({
   cellarId,
   ...rest
 }) => {
+  const isAave = cellarId === "AAVE"
   const cellarData = cellarDataMap[cellarId]
   const cellarConfig = cellarData.config
   const { data: tvm } = useTvm(cellarConfig)
@@ -33,14 +38,65 @@ export const ValueManaged: React.FC<Props> = ({
   const { data: cellarCap } = useCellarCap(cellarConfig)
   const { data: currentDeposits } = useCurrentDeposits(cellarConfig)
   const { data: activeAsset } = useActiveAsset(cellarConfig)
+  const { data: tokenPrice } = useTokenPrice(cellarConfig)
+  const { data: weekChange } = useWeekChange(cellarConfig)
+  const isWeekChangeNegative = weekChange && weekChange < 0
+
+  const valueManagedData = {
+    firstTooltip: isAave
+      ? "Total value managed by Strategy"
+      : "1 token price which is calculated based on current BTC, ETH, and USDC prices vs their proportions in strategy vs minted tokens in strategy",
+    firstLabel: isAave ? "TVM" : "Token price",
+    firstValue: isAave ? tvm?.formatted : tokenPrice,
+    secondTooltip: isAave
+      ? apy?.apyLabel
+      : "% of current token price vs token price 1 W(7 days) ago",
+    secondLabel: isAave ? "Expected APY" : "1W Change",
+    secondValue: isAave ? (
+      <Heading
+        size="sm"
+        display="flex"
+        alignItems="center"
+        columnGap="3px"
+      >
+        {apyLoading ? <Spinner /> : apy?.expectedApy}
+      </Heading>
+    ) : (
+      <>
+        {weekChange ? (
+          <HStack
+            color={isWeekChangeNegative ? "red.base" : "lime.base"}
+            spacing={0}
+          >
+            <Icon
+              as={isWeekChangeNegative ? FaArrowDown : FaArrowUp}
+            />
+            <Heading
+              size="sm"
+              display="flex"
+              alignItems="center"
+              columnGap="3px"
+            >
+              {Math.abs(weekChange).toFixed(2)}%
+            </Heading>
+          </HStack>
+        ) : (
+          <Box>...</Box>
+        )}
+      </>
+    ),
+  }
+
   return (
     <Box {...rest}>
       <Flex alignItems="baseline" mb={1}>
-        <Heading size="md">{tvm?.formatted || "..."}</Heading>
+        <Heading size="md">
+          {valueManagedData.firstValue || "..."}
+        </Heading>
         <Tooltip
           hasArrow
           arrowShadowColor="purple.base"
-          label="Total value managed by Strategy"
+          label={valueManagedData.firstTooltip}
           placement="top"
           bg="surface.bg"
           color="neutral.300"
@@ -53,7 +109,7 @@ export const ValueManaged: React.FC<Props> = ({
               alignItems="center"
               columnGap="4px"
             >
-              TVM
+              {valueManagedData.firstLabel}
             </Label>
 
             <InformationIcon color="neutral.300" boxSize={3} />
@@ -61,25 +117,17 @@ export const ValueManaged: React.FC<Props> = ({
         </Tooltip>
       </Flex>
       <Flex alignItems="center" mb={4}>
-        <Heading
-          size="sm"
-          display="flex"
-          alignItems="center"
-          columnGap="3px"
-        >
-          {cellarData.overrideApy?.value ||
-            (apyLoading ? <Spinner /> : apy?.expectedApy)}
-        </Heading>
+        {valueManagedData.secondValue}
         <Tooltip
           hasArrow
           placement="top"
-          label={cellarData.overrideApy?.tooltip || apy?.apyLabel}
+          label={valueManagedData.secondTooltip}
           bg="surface.bg"
           color="neutral.300"
         >
           <HStack spacing={1} align="center">
             <Label ml={1} color="neutral.300">
-              {cellarData.overrideApy?.title || "Expected APY"}
+              {valueManagedData.secondLabel}
             </Label>
             <InformationIcon color="neutral.300" boxSize={3} />
           </HStack>
