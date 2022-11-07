fc31801da3939baa25785bc83d6c7ea6219289d1
Nur Fikri <codingki@gmail.com>
2022-11-08 02:34:02 +0700

    feat(ui): sell modal ui update

--- src/components/_buttons/WithdrawButton.tsx
+++ src/components/_buttons/WithdrawButton.tsx
@@ -1,8 +1,8 @@
 import { ButtonProps, useDisclosure } from "@chakra-ui/react"
-import { WithdrawModal } from "components/_modals/WithdrawModal"
 import { VFC } from "react"
 import { SecondaryButton } from "./SecondaryButton"
 import { analytics } from "utils/analytics"
+import { WithdrawModal } from "components/_modals/WithdrawModal"
 
 export const WithdrawButton: VFC<ButtonProps> = (props) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -17,11 +17,7 @@ export const WithdrawButton: VFC<ButtonProps> = (props) => {
       <SecondaryButton onClick={onOpen} {...props}>
         Sell
       </SecondaryButton>
-      <WithdrawModal
-        isOpen={isOpen}
-        onClose={closeModal}
-        onCloseProp={onClose}
-      />
+      <WithdrawModal isOpen={isOpen} onClose={closeModal} />
     </>
   )
 }
