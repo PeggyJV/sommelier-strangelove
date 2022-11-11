a7b149aab59d8f708f1995ad987fee0ba82b8b88
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-11-12 04:39:38 +0700

    feat: update cellarDetailCard for 3 iteration

--- src/components/_cards/CellarDetailsCard.tsx
+++ src/components/_cards/CellarDetailsCard.tsx
@@ -1,31 +1,32 @@
 import dynamic from "next/dynamic"
 import {
-  Box,
   BoxProps,
   Circle,
   HStack,
   Text,
   Tooltip,
-  useBreakpointValue,
   useTheme,
   VStack,
   Image,
+  Avatar,
+  SimpleGrid,
+  Stack,
 } from "@chakra-ui/react"
-import { CardDivider } from "components/_layout/CardDivider"
 import { CardHeading } from "components/_typography/CardHeading"
 import { VFC } from "react"
 import { useNivoThemes } from "hooks/nivo"
 import { CardStat } from "components/CardStat"
-import { CardStatRow } from "components/CardStatRow"
 import { InformationIcon, UsdcIcon } from "components/_icons"
 import { TransparentCard } from "./TransparentCard"
 import { tokenConfig } from "data/tokenConfig"
-import { TokenAssets } from "components/TokenAssets"
 import { StrategyBreakdownCard } from "./StrategyBreakdownCard"
 import { StrategyProvider } from "components/StrategyProvider"
-import { useActiveAsset } from "data/hooks/useActiveAsset"
 import { CellarDataMap } from "data/types"
 import { protocolsImage } from "utils/protocolsImagePath"
+import { useTvm } from "data/hooks/useTvm"
+import { useActiveAsset } from "data/hooks/useActiveAsset"
+import { isTokenAssets } from "data/uiConfig"
+import { TokenAssets } from "components/TokenAssets"
 const BarChart = dynamic(
   () => import("components/_charts/BarChart"),
   {
@@ -44,11 +45,6 @@ const CellarDetailsCard: VFC<CellarDetailsProps> = ({
 }) => {
   const { barChartTheme } = useNivoThemes()
   const theme = useTheme()
-  const borderColor = useBreakpointValue({
-    sm: "purple.dark",
-    md: "transparent",
-    lg: "purple.dark",
-  })
   const {
     protocols,
     strategyType,
@@ -63,46 +59,37 @@ const CellarDetailsCard: VFC<CellarDetailsProps> = ({
     strategyAssets?.includes(token.symbol)
   )
   const cellarConfig = cellarDataMap[cellarId].config
+  const { data: tvm } = useTvm(cellarConfig)
   const { data: activeAsset } = useActiveAsset(cellarConfig)
 
   // Unsure why this was necessary? Nivo acts strangely when there are fewer than three args in an index. Could be refined later.
-  const moveColors = (colorTheme: string[]): string[] => {
-    const lastColor = colorTheme.slice(-1)
-    const otherColors = colorTheme.slice(0, -1)
-    return [...lastColor, ...otherColors]
-  }
+  // const moveColors = (colorTheme: string[]): string[] => {
+  //   const lastColor = colorTheme.slice(-1)
+  //   const otherColors = colorTheme.slice(0, -1)
+  //   return [...lastColor, ...otherColors]
+  // }
 
-  const colors = moveColors(barChartTheme)
+  // const colors = moveColors(barChartTheme)
 
   const protocolIcon = protocolsImage[protocols]
 
   return (
     <TransparentCard p={8} overflow="visible">
       <VStack spacing={8} align={{ sm: "unset", md: "stretch" }}>
-        <CardStatRow
-          justify={{ sm: "space-around", md: "flex-start" }}
-          align="flex-start"
-          direction={{ sm: "column", md: "row" }}
-          rowGap={{ sm: 0, md: 4 }}
-          wrap={{ sm: "wrap", lg: "nowrap" }}
-          divider={
-            <CardDivider
-              css={{
-                "&:nth-last-of-type(2)": {
-                  borderColor,
-                },
-              }}
-            />
-          }
-        >
+        <SimpleGrid columns={{ base: 3, lg: 4 }} spacing={6}>
           <CardStat
             label="strategy type"
+            flex={0}
             tooltip={strategyTypeTooltip}
           >
             {strategyType}
           </CardStat>
+          {strategyProvider && (
+            <StrategyProvider strategyProvider={strategyProvider} />
+          )}
           <CardStat
             label="protocols"
+            flex={0}
             tooltip="Protocols in which Strategy operates"
             pr={{ sm: 2, lg: 8 }}
           >
@@ -116,6 +103,7 @@ const CellarDetailsCard: VFC<CellarDetailsProps> = ({
           </CardStat>
           <CardStat
             label="mgmt fee"
+            flex={0}
             tooltip={
               managementFeeTooltip || "Platform management fee"
             }
@@ -130,20 +118,61 @@ const CellarDetailsCard: VFC<CellarDetailsProps> = ({
             {managementFee}
           </CardStat>
           <CardStat
+            label="TVM"
+            flex={0}
+            tooltip="Total value managed by Strategy"
+          >
+            {tvm?.formatted || "..."}
+          </CardStat>
+          <CardStat
+            flex={0}
             label="strategy assets"
             tooltip="Strategy will have exposure to 1 or more of these assets at any given time"
           >
-            <TokenAssets
-              tokens={cellarStrategyAssets}
-              activeAsset={activeAsset?.address || ""}
-              displaySymbol
-            />
+            <HStack>
+              {isTokenAssets(cellarConfig) ? (
+                <TokenAssets
+                  tokens={cellarStrategyAssets}
+                  activeAsset={activeAsset?.address || ""}
+                  displaySymbol
+                />
+              ) : (
+                cellarStrategyAssets.map((asset) => (
+                  <HStack
+                    spacing={1}
+                    alignItems="center"
+                    key={asset.address}
+                  >
+                    <Avatar
+                      boxSize="24px"
+                      borderWidth={2}
+                      borderColor="surface.bg"
+                      src={asset.src}
+                      bg="surface.bg"
+                      _notFirst={{
+                        opacity: 0.65,
+                      }}
+                      _hover={{
+                        opacity: 1,
+                      }}
+                      _groupHover={{
+                        _first: {
+                          opacity: 0.65,
+                        },
+                      }}
+                      _first={{
+                        _hover: {
+                          opacity: "1 !important",
+                        },
+                      }}
+                    />
+                    <Text fontSize="0.625rem">78.34%</Text>
+                  </HStack>
+                ))
+              )}
+            </HStack>
           </CardStat>
-          <VStack
-            width={{ sm: "100%", lg: "unset" }}
-            spacing={2}
-            align="stretch"
-          >
+          <VStack width="lg" spacing={2} align="stretch" maxW="150%">
             <HStack spacing={1} align="center">
               <Tooltip
                 hasArrow
@@ -158,7 +187,7 @@ const CellarDetailsCard: VFC<CellarDetailsProps> = ({
                 </HStack>
               </Tooltip>
             </HStack>
-            <Box h="4px" maxW={{ lg: 318 }}>
+            <Stack h="4px">
               {/* @ts-ignore */}
               <BarChart
                 layout="horizontal"
@@ -169,7 +198,7 @@ const CellarDetailsCard: VFC<CellarDetailsProps> = ({
                 keys={["depositors", "protocol", "strategy provider"]}
                 data={[performanceSplit]}
               />
-            </Box>
+            </Stack>
             <HStack spacing={8}>
               {Object.entries(performanceSplit).map(
                 ([key, value], i) => {
@@ -188,14 +217,12 @@ const CellarDetailsCard: VFC<CellarDetailsProps> = ({
               )}
             </HStack>
           </VStack>
-        </CardStatRow>
+        </SimpleGrid>
+
         <StrategyBreakdownCard
           cellarDataMap={cellarDataMap}
           cellarId={cellarId}
         />
-        {strategyProvider && (
-          <StrategyProvider strategyProvider={strategyProvider} />
-        )}
       </VStack>
     </TransparentCard>
   )
