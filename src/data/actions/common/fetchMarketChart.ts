3e16ff2f66f8337157567717038900a26b48c845
Nur Fikri <codingki@gmail.com>
2022-11-15 00:33:30 +0700

    refactor: split fetch and calculation

---
+++ src/data/actions/common/fetchMarketChart.ts
@@ -0,0 +1,27 @@
+interface MarketChartResponse {
+  prices: [number, number][]
+  market_caps: [number, number][]
+  total_volumes: [number, number][]
+}
+
+export const fetchMarketChart = async (
+  asset: string,
+  day: number,
+  interval: string
+) => {
+  const url = `https://api.coingecko.com/api/v3/coins/${asset}/market_chart?vs_currency=usd&days=${day}&interval=${interval}`
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    const data: MarketChartResponse = await response.json()
+
+    return data
+  } catch (error) {
+    console.warn(error)
+    throw error
+  }
+}
