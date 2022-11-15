7c27d62a6b192c749793e5c24434f97f9ca8f0c4
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-11-16 05:28:47 +0700

    feat: update section component

--- src/components/_layout/Section.tsx
+++ src/components/_layout/Section.tsx
@@ -1,6 +1,6 @@
-import { Box, BoxProps } from '@chakra-ui/react'
-import { VFC } from 'react'
+import { Box, BoxProps } from "@chakra-ui/react"
+import { VFC } from "react"
 
-export const Section: VFC<BoxProps> = props => {
-  return <Box as='section' pb={12} {...props} />
+export const Section: VFC<BoxProps> = (props) => {
+  return <Box as="section" pb={12} px={4} {...props} />
 }
