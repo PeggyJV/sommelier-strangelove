bceb43c5942a7065caf4aed795fe261b037be7f2
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-11-21 17:56:27 +0700

    feat: update cellarDataMap ordering

--- src/data/cellarDataMap.ts
+++ src/data/cellarDataMap.ts
@@ -12,10 +12,10 @@ import {
 let cellarConfig: CellarDataMap
 if (config.cleargate.enabled) {
   cellarConfig = {
-    [config.CONTRACT.ETH_BTC_TREND_CELLAR.SLUG]: ethBtcTrend,
-    [config.CONTRACT.ETH_BTC_MOMENTUM_CELLAR.SLUG]: ethBtcMomentum,
     [config.CONTRACT.STEADY_ETH.SLUG]: steadyEth,
     [config.CONTRACT.STEADY_BTC.SLUG]: steadyBtc,
+    [config.CONTRACT.ETH_BTC_TREND_CELLAR.SLUG]: ethBtcTrend,
+    [config.CONTRACT.ETH_BTC_MOMENTUM_CELLAR.SLUG]: ethBtcMomentum,
     [config.CONTRACT.AAVE_V2_STABLE_CELLAR.SLUG]: aaveStable,
   }
 } else {
