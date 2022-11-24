d42152b68547d77d58a49f6ed243807eed043af9
Elliot Shiu <sandbochs@users.noreply.github.com>
2022-11-24 01:14:28 -0800

    send cellar id and name to deposit events

--- src/utils/landingType.ts
+++ src/utils/landingType.ts
@@ -1,7 +1,9 @@
+const DIRECT = "Direct"
+
 export const landingType = () => {
   const referrer = document.referrer
   if (referrer === "" || !referrer) {
-    return "Direct"
+    return DIRECT
   }
   return referrer
 }
