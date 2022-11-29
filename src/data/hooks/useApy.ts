a8511cde93fa9f252803af50834ed1ad5e43a2be
Nur Fikri <codingki@gmail.com>
2022-11-30 04:10:35 +0700

    Feat/new aave apy calculation (#703)

--- src/data/hooks/useApy.ts
+++ src/data/hooks/useApy.ts
@@ -1,10 +1,14 @@
 import { useQuery } from "@tanstack/react-query"
 import { getApy as getApy_AAVE_V2_STABLE_CELLAR } from "data/actions/CELLAR_V0815/getApy"
-import { getRewardsApy as v0815_getRewardsApy } from "data/actions/CELLAR_V0815/getRewardsApy"
+import { getRewardsApy } from "data/actions/CELLAR_V0815/getRewardsApy"
 import { CellarNameKey, ConfigProps } from "data/types"
-import { CellarStakingV0815, CellarV0815 } from "src/abi/types"
+import { useGet10DaysShareValueQuery } from "generated/subgraph"
+import { CellarV0815, CellarStakingV0815 } from "src/abi/types"
+import { getPrevious10Days } from "utils/calculateTime"
 import { useCreateContracts } from "./useCreateContracts"
 import { useSommelierPrice } from "./useSommelierPrice"
+
+const previous10Days = getPrevious10Days()
 
 export const useApy = (config: ConfigProps) => {
   const { cellarContract, stakerContract } =
@@ -12,45 +16,55 @@ export const useApy = (config: ConfigProps) => {
 
   const sommPrice = useSommelierPrice()
 
-  const v0815GetApy = [CellarNameKey.AAVE]
-  const v0815ApyEnabled = v0815GetApy.includes(config.cellarNameKey)
-  const v0815ApyQueryEnabled = Boolean(
-    v0815ApyEnabled &&
+  const [{ fetching, data, error }, reexecute10Days] =
+    useGet10DaysShareValueQuery({
+      variables: {
+        epoch: previous10Days,
+        cellarAddress: config.id,
+      },
+    })
+
+  const dayDatas = data?.cellar?.dayDatas
+
+  const aaveQueryEnabled = Boolean(
+    config.cellarNameKey === CellarNameKey.AAVE &&
+      dayDatas &&
       cellarContract.provider &&
       stakerContract?.provider
   )
 
-  const v0815GetRewardApy = [
+  const getRewardsApyCellars = [
     CellarNameKey.ETH_BTC_MOM,
     CellarNameKey.ETH_BTC_TREND,
     CellarNameKey.STEADY_BTC,
     CellarNameKey.STEADY_ETH,
   ]
-  const v0815RewardApyEnabled = v0815GetRewardApy.includes(
+  const getRewardsApyEnabled = getRewardsApyCellars.includes(
     config.cellarNameKey
   )
-  const v0815RewardApyQueryEnabled = Boolean(
-    v0815RewardApyEnabled && stakerContract?.provider
+  const getRewardsApyQueryEnabled = Boolean(
+    getRewardsApyEnabled && stakerContract?.provider
   )
 
-  const queryEnabled =
-    v0815ApyQueryEnabled || v0815RewardApyQueryEnabled
+  const queryEnabled = aaveQueryEnabled || getRewardsApyQueryEnabled
 
   const query = useQuery(
-    ["USE_APY", sommPrice.data, config.cellar.address],
+    ["USE_APY", config.cellar.address],
     async () => {
       if (!sommPrice.data) {
         throw new Error("Sommelier price is undefined")
       }
 
-      if (v0815ApyEnabled) {
+      if (config.cellarNameKey === CellarNameKey.AAVE) {
         return await getApy_AAVE_V2_STABLE_CELLAR(
           cellarContract as CellarV0815,
           stakerContract as CellarStakingV0815,
-          sommPrice.data
+          sommPrice.data,
+          dayDatas!
         )
-      } else if (v0815RewardApyQueryEnabled) {
-        return await v0815_getRewardsApy(
+      }
+      if (getRewardsApyEnabled) {
+        return await getRewardsApy(
           stakerContract as CellarStakingV0815,
           sommPrice.data
         )
@@ -65,10 +79,13 @@ export const useApy = (config: ConfigProps) => {
 
   return {
     ...query,
-    isLoading: sommPrice.isLoading || query.isLoading,
-    isFetching: sommPrice.isFetching || query.isFetching,
-    isRefetching: sommPrice.isRefetching || query.isRefetching,
-    isError: sommPrice.isError || query.isError,
-    error: sommPrice.error || query.error,
+    isLoading: fetching || query.isLoading || sommPrice.isLoading,
+    error: error || query.error || sommPrice.error,
+    isError: !!error || query.isError || sommPrice.isError,
+    isFetching: fetching || query.isFetching || sommPrice.isFetching,
+    refetch: () => {
+      reexecute10Days()
+      query.refetch()
+    },
   }
 }
