50ac903ba9a58f757cc302974f20824c27133bf3
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-11-29 07:36:29 +0700

    feat: update getAssetIntervalGain

--- src/data/actions/common/getAssetIntervalGain.ts
+++ src/data/actions/common/getAssetIntervalGain.ts
@@ -9,10 +9,11 @@ export const getAssetIntervalGain = async (
 ) => {
   try {
     const data = await fetchMarketChart(asset, day, "daily")
-    const previousData = data.prices[0]
+    const previousData = data.prices[1]
     // coingecko returns the latest date with 2 hour value, 00:00 data and latest hour data. We get the 00:00 value with length - 2 index
-    const todayIndex = data.prices.length === day + 1 ? -2 : -1
+    const todayIndex = -1
     const todayData = data.prices.at(todayIndex)
+
     if (!todayData) throw new Error("todayData undefined")
     const result = getGainPct(todayData[1], previousData[1])
 
