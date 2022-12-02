cef8b7ee6a9fcc1efe55b0ffd88996c796cf4c8a
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-12-03 01:44:48 +0700

    feat: add mobile warning on bridge page

--- src/components/_pages/PageBridge.tsx
+++ src/components/_pages/PageBridge.tsx
@@ -3,11 +3,16 @@ import { Layout } from "components/Layout"
 import { Center } from "@chakra-ui/react"
 
 import { BridgeCard } from "components/_cards/BridgeCard"
+import { MobileWarningCTA } from "components/MobileWarningCTA"
 
 export const PageBridge: NextPage = () => {
   return (
     <Layout>
-      <Center>
+      <MobileWarningCTA
+        display={{ base: "flex", md: "none" }}
+        text="Bridge UI is not supported by mobile yet. Please use a desktop for Bridge UI"
+      />
+      <Center display={{ base: "none", md: "flex" }}>
         <BridgeCard />
       </Center>
     </Layout>
