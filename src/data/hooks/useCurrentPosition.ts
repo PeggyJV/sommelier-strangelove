4ef96c55489926cc8b019ef96b5e7264add39bfb
Elliot Shiu <sandbochs@users.noreply.github.com>
2022-12-07 12:13:08 -0600

    cleanup withdrawable asset calculation for 1.5 cellars

--- src/data/hooks/useCurrentPosition.ts
+++ src/data/hooks/useCurrentPosition.ts
@@ -1,11 +1,24 @@
 import { CellarKey, ConfigProps } from "data/types"
-import { ContractInterface, ethers } from "ethers"
+import { ethers } from "ethers"
 import { CellarV0816 } from "src/abi/types"
 import { useAccount, useQuery, useSigner } from "wagmi"
 import { useCreateContracts } from "./useCreateContracts"
 import erc20 from "src/abi/erc20.json"
 import { useUserBalances } from "./useUserBalances"
 import BigNumber from "bignumber.js"
+import { config } from "src/utils/config"
+
+const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY
+const provider = new ethers.providers.AlchemyProvider(
+  "homestead",
+  alchemyKey
+)
+const usdcAddress = config.CONTRACT.USDC.ADDRESS
+const ERC20 = new ethers.Contract(usdcAddress, erc20, provider)
+
+function toBN(value: any) {
+  return new BigNumber(value.toString())
+}
 
 export const useCurrentPosition = (config: ConfigProps) => {
   const { cellarContract } = useCreateContracts(config)
@@ -27,27 +40,30 @@ export const useCurrentPosition = (config: ConfigProps) => {
         if (!address) throw new Error("address is undefined")
         if (config.cellar.key === CellarKey.CELLAR_V0816) {
           const contract: CellarV0816 = cellarContract
+          // Returns max number of shares withdrawable by user
+          // Will always be <= total user shares
           const maxSharesOut = await contract
             .maxWithdraw(address)
-            .then((v) => new BigNumber(v.toString())) // Returns # of shares withdrawable by user, <= total user shares
-          const totalShares = await contract
-            .totalSupply()
-            .then((v) => new BigNumber(v.toString())) // Total shares across all users
+            .then(toBN) // Total shares across all users
 
+          // Total shares minted by the Cellar
+          const totalShares = await contract.totalSupply().then(toBN)
+
+          // Positions held by the cellar
           const positionAddresses = await contract.getPositions()
+
+          // Get balance of each position held by the Cellar
           const positionBalances = await Promise.all(
             positionAddresses.map(async (position) => {
-              const c = new ethers.Contract(
-                position,
-                erc20 as ContractInterface,
-                signer?.provider
-              )
+              // Attach to ERC20 contract
+              const c = ERC20.attach(position)
+
               const cellarBalance: BigNumber = await c
                 .balanceOf(config.cellar.address)
-                .then(
-                  (v: ethers.BigNumber) => new BigNumber(v.toString())
-                )
-              const decimals: number = await c.decimals()
+                .then(toBN)
+              const decimals: number = await c
+                .decimals()
+                .then((v: ethers.BigNumber) => v.toNumber())
 
               return {
                 address: position,
@@ -57,36 +73,23 @@ export const useCurrentPosition = (config: ConfigProps) => {
             })
           )
 
-          let totalWithdrawable = new BigNumber(0)
-          positionBalances.map((item) => {
-            totalWithdrawable = totalWithdrawable.plus(
-              new BigNumber(item.balance)
-                .multipliedBy(maxSharesOut)
-                .div(totalShares)
-            )
+          // This percentage is the same across all positions held by the cellar
+          const percentage = maxSharesOut.div(totalShares).times(100)
+          return positionBalances.map((position) => {
+            // Cellar balance of position
+            const { balance } = position
+
+            // User balance = user withdrawable percentage * cellar balance
+            const withdrawable = balance
+              .times(maxSharesOut)
+              .div(totalShares)
+
+            return {
+              ...position,
+              withdrawable,
+              percentage,
+            }
           })
-
-          const result = await Promise.all(
-            positionBalances.map(async (item) => {
-              const withdrawable = new BigNumber(item.balance)
-                .multipliedBy(maxSharesOut)
-                .div(totalShares)
-
-              const ratio = new BigNumber(withdrawable).div(
-                totalWithdrawable
-              )
-
-              return {
-                address: item.address,
-                balance: item.balance,
-                decimals: item.decimals,
-                withdrawable: withdrawable.multipliedBy(item.balance),
-                percentage: ratio.multipliedBy(100),
-              }
-            })
-          )
-
-          return result
         }
 
         throw new Error("UNKNOWN CONTRACT")
