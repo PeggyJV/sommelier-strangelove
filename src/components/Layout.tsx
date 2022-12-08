74663ed387d33c9ac1a9e492be642e7122f3f82b
Nur Fikri <codingki@gmail.com>
2022-12-09 04:49:03 +0700

    feat: remove geo banner

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,13 @@
 import { VFC } from "react"
 import { Box, Container, Flex, FlexProps } from "@chakra-ui/react"
-import { useGeo } from "context/geoContext"
 import { BackgroundAssets } from "./BackgroundAssets"
 import { Nav } from "./Nav"
-import { GeoBanner } from "./_banners/GeoBanner"
 import Footer from "./Footer"
 import { useAccount, useNetwork } from "wagmi"
 import { WrongNetworkBanner } from "./_banners/WrongNetworkBanner"
 import { useIsMounted } from "hooks/utils/useIsMounted"
 
 export const Layout: VFC<FlexProps> = ({ children, ...rest }) => {
-  const { isRestricted } = useGeo() || {}
   const { isConnected } = useAccount()
   const { chain } = useNetwork()
   const isMounted = useIsMounted()
@@ -28,7 +25,6 @@ export const Layout: VFC<FlexProps> = ({ children, ...rest }) => {
             maxW="container.lg"
             px={{ base: 0, sm: 4 }}
           >
-            {isRestricted && <GeoBanner />}
             {isMounted && isConnected && chain?.id !== 1 && (
               <WrongNetworkBanner />
             )}
