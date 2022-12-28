3388f3ed04645fc720275db0e8df3e397f9a40ec
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2022-12-28 14:04:50 +0700

    feat: remove isTradedAssetsHardCoded

--- src/data/uiConfig.ts
+++ src/data/uiConfig.ts
@@ -25,15 +25,6 @@ export const isTokenAssets = (config: ConfigProps) => {
   return config.cellarNameKey === CellarNameKey.AAVE
 }
 
-export const isTradedAssetsHardCoded = (config: ConfigProps) => {
-  if (config.cellarNameKey === CellarNameKey.STEADY_UNI) {
-    return ["USDC", "UNI"]
-  } else if (config.cellarNameKey === CellarNameKey.STEADY_MATIC) {
-    return ["USDC", "MATIC"]
-  }
-  return null
-}
-
 export const intervalGainTimeline = (config: ConfigProps) => {
   if (
     config.cellarNameKey === CellarNameKey.ETH_BTC_MOM ||
