d94d8b6afba43c63aa39dc3bdd1e7602b4082124
Nur Fikri <codingki@gmail.com>
2023-01-12 22:49:20 +0700

    feat: improve types

--- src/hooks/web3/useImportToken.tsx
+++ src/hooks/web3/useImportToken.tsx
@@ -12,54 +12,54 @@ type UseImportTokenArgs = MutationEventArgs<
   Awaited<ReturnType<typeof fetchToken>>
 >
 
+type DoImportToken = (
+  args: Args
+) => Promise<Awaited<ReturnType<typeof fetchToken>>>
+
+export const doImportToken: DoImportToken = async ({
+  address,
+  imageUrl,
+}) => {
+  try {
+    if (typeof window.ethereum === "undefined") {
+      throw new Error("No wallet installed")
+    }
+    const tokenData = await fetchToken({
+      address,
+    })
+    if (!tokenData) {
+      throw new Error("Token data is undefined")
+    }
+    const imgUrl = Object.values(cellarDataMap).find(
+      (item) => item.config.lpToken.address === address
+    )?.config.lpToken.imagePath
+    const fullImageUrl = `${window.origin}${imgUrl}`
+    const res = await window.ethereum.request({
+      method: "wallet_watchAsset",
+      params: {
+        type: "ERC20",
+        options: {
+          address: address,
+          symbol: tokenData.symbol,
+          decimals: tokenData.decimals,
+          image: imageUrl || fullImageUrl,
+        },
+      },
+    })
+    if (!res) {
+      throw new Error("Failed to import token")
+    }
+    return tokenData
+  } catch (e) {
+    const error = e as Error
+    throw error
+  }
+}
 export const useImportToken = ({
   onSuccess,
   onError,
   onMutate,
 }: UseImportTokenArgs = {}) => {
-  const doImportToken = async ({
-    address,
-    imageUrl,
-  }: {
-    address: string
-    imageUrl?: string
-  }) => {
-    try {
-      if (typeof window.ethereum === "undefined") {
-        throw new Error("No wallet installed")
-      }
-      const tokenData = await fetchToken({
-        address,
-      })
-      if (!tokenData) {
-        throw new Error("Token data is undefined")
-      }
-      const imgUrl = Object.values(cellarDataMap).find(
-        (item) => item.config.lpToken.address === address
-      )?.config.lpToken.imagePath
-      const fullImageUrl = `${window.origin}${imgUrl}`
-      const res = await window.ethereum.request({
-        method: "wallet_watchAsset",
-        params: {
-          type: "ERC20",
-          options: {
-            address: address,
-            symbol: tokenData.symbol,
-            decimals: tokenData.decimals,
-            image: imageUrl || fullImageUrl,
-          },
-        },
-      })
-      if (!res) {
-        throw new Error("Failed to import token")
-      }
-      return tokenData
-    } catch (e) {
-      const error = e as Error
-      throw error
-    }
-  }
-
   const query = useMutation(["USE_IMPORT_TOKEN"], doImportToken, {
     onSuccess,
     onError,
