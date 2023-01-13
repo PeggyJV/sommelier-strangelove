9109c697ea624fd59f26c278a69c356860cd0194
Nur Fikri <codingki@gmail.com>
2023-01-14 01:31:44 +0700

    feat: import reward token after claim and extra refetch

--- src/components/_cards/PortfolioCard/Rewards.tsx
+++ src/components/_cards/PortfolioCard/Rewards.tsx
@@ -1,14 +1,19 @@
-import { SimpleGrid, VStack } from "@chakra-ui/react"
+import { SimpleGrid, VStack, Text } from "@chakra-ui/react"
 import { CardStat } from "components/CardStat"
 import { InlineImage } from "components/InlineImage"
+import { Link } from "components/Link"
 import { BaseButton } from "components/_buttons/BaseButton"
+import { ExternalLinkIcon } from "components/_icons"
 import { useGeo } from "context/geoContext"
 import { useCreateContracts } from "data/hooks/useCreateContracts"
 import { useUserStakes } from "data/hooks/useUserStakes"
 import { ConfigProps } from "data/types"
+import { useBrandedToast } from "hooks/chakra"
 import { useIsMounted } from "hooks/utils/useIsMounted"
 import { useHandleTransaction } from "hooks/web3"
+import { useImportToken } from "hooks/web3/useImportToken"
 import { analytics } from "utils/analytics"
+import { config } from "utils/config"
 import { useAccount } from "wagmi"
 
 export const Rewards = ({
@@ -20,7 +25,27 @@ export const Rewards = ({
   const { isConnected } = useAccount()
   const { data: userStakes, refetch } = useUserStakes(cellarConfig)
   const { stakerSigner } = useCreateContracts(cellarConfig)
+  const { addToast, close } = useBrandedToast()
 
+  const importToken = useImportToken({
+    onSuccess: (data) => {
+      addToast({
+        heading: "Import Token",
+        status: "success",
+        body: <Text>{data.symbol} added to metamask</Text>,
+        closeHandler: close,
+      })
+    },
+    onError: (error) => {
+      const e = error as Error
+      addToast({
+        heading: "Import Token",
+        status: "error",
+        body: <Text>{e.message}</Text>,
+        closeHandler: close,
+      })
+    },
+  })
   const userRewards =
     userStakes?.totalClaimAllRewards?.value.toString()
 
@@ -38,8 +63,42 @@ export const Rewards = ({
     const tx = await stakerSigner?.claimAll()
     await doHandleTransaction({
       ...tx,
-      onSuccess: () => analytics.track("rewards.claim-succeeded"),
+      onSuccess: () => {
+        refetch()
+        analytics.track("rewards.claim-succeeded")
+      },
       onError: () => analytics.track("rewards.claim-failed"),
+      toastBody: {
+        successWithParams: (result) => {
+          const fullImageUrl = `${window.origin}${config.CONTRACT.SOMMELLIER.IMAGE_PATH}`
+          return (
+            <>
+              <Text>Successful</Text>
+              <Link
+                display="flex"
+                alignItems="center"
+                href={`https://etherscan.io/tx/${result?.data?.transactionHash}`}
+                isExternal
+              >
+                <Text as="span">View on Etherscan</Text>
+                <ExternalLinkIcon ml={2} />
+              </Link>
+              <Text
+                onClick={() => {
+                  importToken.mutate({
+                    address: config.CONTRACT.SOMMELLIER.ADDRESS,
+                    imageUrl: fullImageUrl,
+                  })
+                }}
+                textDecor="underline"
+                as="button"
+              >
+                Import Reward token to wallet
+              </Text>
+            </>
+          )
+        },
+      },
     })
     refetch()
   }
