45a0999169abe00b4abe8f009271234195c5c063
Nur Fikri <codingki@gmail.com>
2023-01-26 00:45:09 +0700

    feat: tvm fetch from contract

--- src/data/hooks/useTvm.ts
+++ src/data/hooks/useTvm.ts
@@ -1,38 +1,42 @@
 import { useQuery } from "@tanstack/react-query"
-import { getTvm } from "data/actions/common/getTvm"
+import { getTotalAssets } from "data/actions/common/getTotalAssets"
 import { CellarKey, ConfigProps } from "data/types"
-import { useGetCellarQuery } from "generated/subgraph"
 import { useCreateContracts } from "./useCreateContracts"
 
 export const useTvm = (config: ConfigProps) => {
   const { cellarContract } = useCreateContracts(config)
 
-  const [cellarResult] = useGetCellarQuery({
-    variables: {
-      cellarAddress: config.id,
-      cellarString: config.id,
-    },
-  })
+  // const [cellarResult] = useGetCellarQuery({
+  //   variables: {
+  //     cellarAddress: config.id,
+  //     cellarString: config.id,
+  //   },
+  // })
 
-  const { data } = cellarResult
-  const { cellar } = data || {}
-  const { tvlTotal } = cellar || {}
+  // const { data } = cellarResult
+  // const { cellar } = data || {}
+  // const { tvlTotal } = cellar || {}
 
   const queryEnabled = Boolean(
     (config.cellar.key === CellarKey.CELLAR_V0815 ||
       config.cellar.key === CellarKey.CELLAR_V0816) &&
-      cellarContract.provider &&
-      tvlTotal
+      cellarContract.provider
+    // &&
+    // tvlTotal
   )
 
   const query = useQuery(
-    ["USE_TVM", tvlTotal, config.cellar.address],
+    [
+      "USE_TVM",
+      // tvlTotal,
+      config.cellar.address,
+    ],
     async () => {
       if (
         config.cellar.key === CellarKey.CELLAR_V0815 ||
         config.cellar.key === CellarKey.CELLAR_V0816
       ) {
-        return await getTvm(tvlTotal)
+        return await getTotalAssets(cellarContract)
       }
       throw new Error("UNKNOWN CONTRACT")
     },
