77c3ec4e387719c6fe41060a11b3f3549ff13690
Nur Fikri <codingki@gmail.com>
2023-02-02 08:07:27 +0700

    feat: reorder nav

--- src/utils/navLinks.ts
+++ src/utils/navLinks.ts
@@ -3,12 +3,16 @@ import { BRIDGE_PAGE_ENABLED } from "./constants"
 export const NAV_LINKS = (() => {
   const links = [
     {
-      link: "https://www.sommelier.finance/",
-      title: "About",
+      link: "/",
+      title: "Strategies",
     },
     {
       link: "/bridge",
       title: "Bridge",
+    },
+    {
+      link: "https://www.sommelier.finance/",
+      title: "About",
     },
   ]
   if (!BRIDGE_PAGE_ENABLED) {
