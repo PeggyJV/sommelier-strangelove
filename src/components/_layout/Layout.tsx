0aae141feabe0246ece90fb9fccb61629d30b128
Nur Fikri <codingki@gmail.com>
2023-02-07 02:13:39 +0700

    feat: layout v2 component

---
+++ src/components/_layout/Layout.tsx
@@ -0,0 +1,40 @@
+import { Box, Container, Flex } from "@chakra-ui/react"
+import Footer from "components/Footer"
+import { Nav } from "components/Nav"
+import { Sidebar } from "components/_sidebar"
+import { rest } from "lodash"
+import { FC } from "react"
+import { useAccount } from "wagmi"
+
+export const Layout: FC = ({ children }) => {
+  const { isConnected } = useAccount()
+  return (
+    <Box>
+      <Box display="block">
+        <Flex minH="100vh" bg="#1A1A23" flexDir="column" {...rest}>
+          <Nav />
+          <Container
+            as="main"
+            flex={1}
+            pt={40}
+            maxW="container.lg"
+            px={{ base: 4, lg: 4 }}
+            flexDir="row"
+          >
+            <Flex wrap="wrap-reverse" gap={{ base: "44px", lg: 8 }}>
+              <Box w={{ base: "full", lg: "700px" }} flex={7}>
+                {children}
+              </Box>
+              {isConnected && (
+                <Box minW={{ base: "full", lg: "300px" }} flex={3}>
+                  <Sidebar />
+                </Box>
+              )}
+            </Flex>
+          </Container>
+        </Flex>
+        <Footer />
+      </Box>
+    </Box>
+  )
+}
