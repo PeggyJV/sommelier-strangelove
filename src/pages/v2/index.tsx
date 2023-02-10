5cb6ab9a64df841ba30de8d5558a7865c4ffbc4d
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-02-11 05:52:17 +0700

    feat: add dummy data for preview

--- src/pages/v2/index.tsx
+++ src/pages/v2/index.tsx
@@ -3,9 +3,35 @@ import { StrategyTable } from "components/_tables/StrategyTable"
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
+  const strategies = [
+    {
+      title: "Real Yield USD",
+      type: "Yield",
+      provider: "Seven Seas",
+      launchDate: new Date("2023-01-25T00:00:00.000Z"),
+      protocols: ["AAVE", "Compound", "Uniswap V3"],
+      strategyAssets: ["USDC", "USDT", "DAI"],
+      tvm: "$1.99M",
+      baseApy: "20%",
+      oneDay: 0.03,
+      icon: "/assets/icons/real-yield-usd.png",
+    },
+    {
+      title: "Steady UNI",
+      type: "Portofolio",
+      provider: "Patache",
+      launchDate: new Date(2022, 11, 29, 11, 0, 0, 0),
+      protocols: ["Uniswap V3"],
+      strategyAssets: ["USDC", "UNI"],
+      tvm: "$1.99M",
+      baseApy: "20%",
+      oneDay: 0.01,
+      icon: "/assets/icons/steady-uni.png",
+    },
+  ]
   return (
     <Layout>
-      <StrategyTable />
+      <StrategyTable strategies={strategies} />
     </Layout>
   )
 }
