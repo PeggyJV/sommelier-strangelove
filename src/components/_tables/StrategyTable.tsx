20b23ff74f02c586b40e4e089c1cef73b6d5539c
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-02-17 21:27:52 +0700

    feat: update to use react table

--- src/components/_tables/StrategyTable.tsx
+++ src/components/_tables/StrategyTable.tsx
@@ -1,27 +1,19 @@
 import {
-  Avatar,
-  AvatarGroup,
-  Box,
-  Flex,
-  Heading,
-  Image,
-  Stack,
   Table,
   TableCellProps,
   TableContainer,
   TableRowProps,
   Tbody,
   Td,
-  Text,
   Th,
   Thead,
   Tr,
 } from "@chakra-ui/react"
 import { VFC } from "react"
 
-import { tokenConfig } from "data/tokenConfig"
+import { useTable, useSortBy } from "react-table"
+import { getAllStrategiesData } from "data/actions/common/getAllStrategiesData"
 
-import { protocolsImage } from "utils/protocolsImagePath"
 export const BorderTr: VFC<TableRowProps> = (props) => {
   return (
     <Tr
@@ -58,122 +50,65 @@ export const BorderTd: VFC<TableCellProps> = (props) => {
   return <Td {...props} />
 }
 
-export type StrategyTableProps = {
-  strategies: {
-    title: string
-    type: string
-    provider: string
-    launchDate: Date
-    protocols: string[]
-    strategyAssets: string[]
-    tvm: string
-    baseApy: string
-    oneDay: number
-    icon: string
-  }[]
+export interface StrategyTableProps {
+  data: Awaited<ReturnType<typeof getAllStrategiesData>>
+  columns: any
 }
 
 export const StrategyTable: VFC<StrategyTableProps> = ({
-  strategies,
+  columns,
+  data,
 }) => {
-  const getTradedAssets = (item: string) => {
-    const asset = tokenConfig.find((v) => v.symbol === item)
-    return asset
-  }
-  const getProtocols = (protocols: string) => {
-    return {
-      title: protocols,
-      icon: protocolsImage[protocols],
-    }
-  }
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  )
   return (
     <TableContainer>
-      <Table
-        variant="unstyled"
-        sx={{
-          borderCollapse: "collapse",
-        }}
-      >
-        <Thead border="none" color="neutral.400">
-          <BorderTr>
-            <Th textTransform="unset">Strategy</Th>
-            <Th textTransform="unset">Protocols</Th>
-            <Th textTransform="unset">Assets</Th>
-            <Th textTransform="unset">TVM</Th>
-            <Th textTransform="unset">Base APY</Th>
-            <Th textTransform="unset">1D</Th>
-          </BorderTr>
-        </Thead>
-        <Tbody backgroundColor="surface.primary">
-          {strategies.map((strategy) => (
-            <BorderTr key={strategy.title}>
-              <BorderTd>
-                <Stack direction="row" alignItems="center">
-                  <Image
-                    boxSize="45px"
-                    src={strategy.icon}
-                    rounded="full"
-                    alt="strategy icon"
-                  />
-                  <Box>
-                    <Heading fontSize="1rem">
-                      {strategy.title}
-                    </Heading>
-                    <Flex
-                      gap={1}
-                      alignItems="center"
-                      fontSize="0.75rem"
-                      fontWeight={600}
-                    >
-                      <Text
-                        bg="rgba(78, 56, 156, 0.32)"
-                        px={1.5}
-                        rounded="4"
-                      >
-                        In 3 days
-                      </Text>
-                      <Text color="neutral.400">
-                        Patache.Portofolio
-                      </Text>
-                    </Flex>
-                  </Box>
-                </Stack>
-              </BorderTd>
-              <BorderTd>
-                <AvatarGroup size="sm" max={3}>
-                  {strategy.protocols.map((protocol) => {
-                    const data = getProtocols(protocol)
-                    return (
-                      <Avatar
-                        name={data.title}
-                        src={data.icon}
-                        key={data.title}
-                      />
-                    )
-                  })}
-                </AvatarGroup>
-              </BorderTd>
-              <BorderTd>
-                <AvatarGroup size="sm" max={3}>
-                  {strategy.strategyAssets.map((protocol) => {
-                    const data = getTradedAssets(protocol)
-                    return (
-                      <Avatar
-                        name={data?.symbol}
-                        src={data?.src}
-                        key={data?.symbol}
-                      />
-                    )
-                  })}
-                </AvatarGroup>
-              </BorderTd>
-              <BorderTd fontWeight={600} fontSize="12px">
-                {strategy.tvm}
-              </BorderTd>
-              <BorderTd>{strategy.baseApy}</BorderTd>
-              <BorderTd>{strategy.oneDay}%</BorderTd>
-            </BorderTr>
+      <Table {...getTableProps()}>
+        <Thead>
+          {headerGroups.map((headerGroup, index) => (
+            <Tr {...headerGroup.getHeaderGroupProps()} key={index}>
+              {headerGroup.headers.map((column, index) => (
+                <Th
+                  userSelect="none"
+                  textTransform="unset"
+                  {...column.getHeaderProps()}
+                  key={index}
+                >
+                  {column.render("Header")}
+                </Th>
+              ))}
+            </Tr>
           ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {rows.map((row, indexRow) => {
+            prepareRow(row)
+            return (
+              <BorderTr {...row.getRowProps()} key={indexRow}>
+                {row.cells.map((cell, indexData) => {
+                  return (
+                    <BorderTd
+                      {...cell.getCellProps()}
+                      key={indexData}
+                    >
+                      {cell.render("Cell")}
+                    </BorderTd>
+                  )
+                })}
+              </BorderTr>
+            )
+          })}
         </Tbody>
       </Table>
     </TableContainer>
