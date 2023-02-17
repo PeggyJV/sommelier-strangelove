de8216c888f91a9063155dabc28c621932502221
Nur Fikri <codingki@gmail.com>
2023-02-17 23:38:07 +0700

    feat: change key

--- src/data/hooks/useAllStrategiesData.ts
+++ src/data/hooks/useAllStrategiesData.ts
@@ -1,20 +1,17 @@
 import { useQuery } from "@tanstack/react-query"
 import { getAllStrategiesData } from "data/actions/common/getAllStrategiesData"
-import { useProvider, useSigner } from "wagmi"
+import { useProvider } from "wagmi"
 import { useAllContracts } from "./useAllContracts"
 import { useCoinGeckoPrice } from "./useCoinGeckoPrice"
 
 export const useAllStrategiesData = () => {
-  const { data: signer } = useSigner()
   const provider = useProvider()
 
   const { data: allContracts } = useAllContracts()
   const { data: sommPrice } = useCoinGeckoPrice("sommelier")
+
   const query = useQuery(
-    [
-      "USE_ALL_STRATEGIES_DATA",
-      { signer: signer?._isSigner, provider: provider?._isProvider },
-    ],
+    ["USE_ALL_STRATEGIES_DATA", { provider: provider?._isProvider }],
     async () => {
       return await getAllStrategiesData({
         allContracts: allContracts!,
