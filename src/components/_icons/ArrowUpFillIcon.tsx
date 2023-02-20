7da5fbbcd8179058c1e6d16a3a4fd32e8622a034
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-02-21 03:03:31 +0700

    feat: create other arrow type

---
+++ src/components/_icons/ArrowUpFillIcon.tsx
@@ -0,0 +1,11 @@
+import { Icon, IconProps } from "@chakra-ui/react"
+import { VFC } from "react"
+
+export const ArrowUpFillIcon: VFC<IconProps> = (props) => (
+  <Icon viewBox="0 0 8 8" {...props}>
+    <path
+      d="M7.914 6.406L4.486.977a.629.629 0 00-1.006 0L.051 6.406a.571.571 0 000 .571.606.606 0 00.509.28h6.869a.606.606 0 00.508-.28.571.571 0 00-.023-.571z"
+      fill="#BCE051"
+    />
+  </Icon>
+)
