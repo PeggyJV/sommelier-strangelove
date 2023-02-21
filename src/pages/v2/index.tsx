f15ef9d3f460e7e259a395f1a793ff2e5a066c08
Nur Fikri <codingki@gmail.com>
2023-02-22 00:58:31 +0700

    change to useBetterMediaQuery

--- src/pages/v2/index.tsx
+++ src/pages/v2/index.tsx
@@ -1,14 +1,15 @@
-import { Heading, useMediaQuery } from "@chakra-ui/react"
+import { Heading } from "@chakra-ui/react"
 import { StrategyDesktopColumn } from "components/_columns/StrategyDesktopColumn"
 import { StrategyMobileColumn } from "components/_columns/StrategyMobileColumn"
 import { Layout } from "components/_layout/Layout"
 import { StrategyTable } from "components/_tables/StrategyTable"
 import { useAllStrategiesData } from "data/hooks/useAllStrategiesData"
+import useBetterMediaQuery from "hooks/utils/useBetterMediaQuery"
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
   const { data } = useAllStrategiesData()
-  const [isLargerThan768] = useMediaQuery("(min-width: 768px)")
+  const isLargerThan768 = useBetterMediaQuery("(min-width: 768px)")
 
   const columns = isLargerThan768
     ? StrategyDesktopColumn
