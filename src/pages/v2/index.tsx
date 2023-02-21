1c4c4bd9acabcfb17e46a6eba81622fde7563041
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-02-21 17:35:08 +0700

    feat: implement responsive table for desktop and mobile

--- src/pages/v2/index.tsx
+++ src/pages/v2/index.tsx
@@ -1,125 +1,19 @@
-import { Avatar, AvatarGroup, Heading, Text } from "@chakra-ui/react"
-import { PercentageText } from "components/PercentageText"
+import { Heading, useMediaQuery } from "@chakra-ui/react"
+import { StrategyDesktopColumn } from "components/_columns/StrategyDesktopColumn"
+import { StrategyMobileColumn } from "components/_columns/StrategyMobileColumn"
 import { Layout } from "components/_layout/Layout"
-import { StrategySection } from "components/_tables/StrategySection"
 import { StrategyTable } from "components/_tables/StrategyTable"
 import { useAllStrategiesData } from "data/hooks/useAllStrategiesData"
-import { tokenConfig } from "data/tokenConfig"
 import type { NextPage } from "next"
-import { CellValue } from "react-table"
-import { protocolsImage } from "utils/protocolsImagePath"
 
 const Home: NextPage = () => {
   const { data } = useAllStrategiesData()
-  const getTradedAssets = (item: string) => {
-    const asset = tokenConfig.find((v) => v.symbol === item)
-    return asset
-  }
-  const getProtocols = (protocols: string) => {
-    return {
-      title: protocols,
-      icon: protocolsImage[protocols],
-    }
-  }
+  const [isLargerThan768] = useMediaQuery("(min-width: 768px)")
 
-  const columns = [
-    {
-      Header: "Strategy",
-      accessor: "name",
-      Cell: ({ row }: any) => {
-        return (
-          <StrategySection
-            icon={row.original.logo}
-            title={row.original.name}
-            provider={row.original.provider.title}
-            type={row.original.type}
-          />
-        )
-      },
-      disableSortBy: true,
-    },
-    {
-      Header: "Protocol",
-      accessor: "protocols",
-      Cell: ({ cell: { value } }: CellValue) => {
-        const protocols = typeof value === "string" ? [value] : value
-        return (
-          <AvatarGroup size="sm" max={3}>
-            {protocols.map((protocol: string) => {
-              const data = getProtocols(protocol)
-              return (
-                <Avatar
-                  name={data.title}
-                  src={data.icon}
-                  key={data.title}
-                  bgColor="white"
-                />
-              )
-            })}
-          </AvatarGroup>
-        )
-      },
-      disableSortBy: true,
-    },
-    {
-      Header: "Assets",
-      accessor: "tradedAssets",
-      Cell: ({ cell: { value } }: CellValue) => {
-        const assets = value?.map((v: any) => v.symbol)
-        if (!assets)
-          return (
-            <Text fontWeight={600} fontSize="12px">
-              --
-            </Text>
-          )
-        return (
-          <AvatarGroup size="sm" max={3}>
-            {assets?.map((asset: string) => {
-              const data = getTradedAssets(asset)
-              return (
-                <Avatar
-                  name={data?.symbol}
-                  src={data?.src}
-                  key={data?.symbol}
-                />
-              )
-            })}
-          </AvatarGroup>
-        )
-      },
-      disableSortBy: true,
-    },
-    {
-      Header: "TVM",
-      accessor: "tvm.value",
-      Cell: ({ row }: any) => (
-        <Text fontWeight={600} fontSize="12px">
-          {row.original.tvm.formatted}
-        </Text>
-      ),
-    },
-    {
-      Header: () => (
-        <Text>
-          Base APY
-          <br />+ Rewards
-        </Text>
-      ),
-      accessor: "baseApy",
-      Cell: ({ cell: { value } }: CellValue) => (
-        <Text fontWeight={600} fontSize="12px">
-          {value?.formatted || "--"}
-        </Text>
-      ),
-    },
-    {
-      Header: "1D",
-      accessor: "changes.daily",
-      Cell: ({ cell: { value } }: CellValue) => (
-        <PercentageText data={value} arrowT2 fontWeight={600} />
-      ),
-    },
-  ]
+  const columns = isLargerThan768
+    ? StrategyDesktopColumn
+    : StrategyMobileColumn
+
   return (
     <Layout>
       <Heading fontSize="1.3125rem" mb="1.6rem">
