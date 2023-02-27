3e14692c26e8d7758566c61b256055754ddb1997
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-02-28 00:45:50 +0700

    feat: update StrategySection component

--- src/components/_tables/StrategySection.tsx
+++ src/components/_tables/StrategySection.tsx
@@ -2,21 +2,25 @@ import {
   Box,
   Flex,
   Heading,
+  HStack,
   Image,
   Stack,
   Text,
   Tooltip,
 } from "@chakra-ui/react"
+import { LogoIcon } from "components/_icons"
 import React from "react"
 import { StrategyDate } from "./StrategyDate"
 
 type StrategySectionProps = {
   icon: string
   title: string
-  provider: string
-  type: number
   description: string
+  provider?: string
+  type?: number
   date?: string
+  netValue?: string
+  rewards?: string
 }
 
 export const StrategySection: React.FC<StrategySectionProps> = ({
@@ -26,6 +30,8 @@ export const StrategySection: React.FC<StrategySectionProps> = ({
   type,
   description,
   date,
+  netValue,
+  rewards,
 }) => {
   const strategyType = type === 0 ? "Yield" : "Portfolio"
   return (
@@ -55,10 +61,34 @@ export const StrategySection: React.FC<StrategySectionProps> = ({
             fontSize="0.75rem"
             fontWeight={600}
           >
-            <StrategyDate date={date} />
-            <Text color="neutral.400">
-              {provider}.{strategyType}
-            </Text>
+            {date && <StrategyDate date={date} />}
+            {provider && strategyType && (
+              <Text color="neutral.400">
+                {provider}.{strategyType}
+              </Text>
+            )}
+            {netValue && (
+              <Text
+                color="neutral.400"
+                fontSize="16px"
+                fontWeight={500}
+              >
+                {netValue}
+              </Text>
+            )}
+            {rewards && rewards !== "0.00" && (
+              <HStack spacing={1} ml={2}>
+                <LogoIcon
+                  mx="auto"
+                  color="red.normal"
+                  p={0}
+                  boxSize="9px"
+                />
+                <Text color="neutral.400" fontSize="12px">
+                  {rewards}
+                </Text>
+              </HStack>
+            )}
           </Flex>
         </Box>
       </Stack>
