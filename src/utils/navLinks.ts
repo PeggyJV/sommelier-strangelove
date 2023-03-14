5983288274dd6831fcbd25dc4b0d92ff5ee32349
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-03-15 03:02:45 +0700

    feat: add audits link

--- src/utils/navLinks.ts
+++ src/utils/navLinks.ts
@@ -14,6 +14,10 @@ export const NAV_LINKS = (() => {
       link: "https://www.sommelier.finance/",
       title: "About",
     },
+    {
+      link: "https://www.sommelier.finance/audits",
+      title: "Audits",
+    },
   ]
   if (!BRIDGE_PAGE_ENABLED) {
     return links.filter((item) => item.title !== "Bridge")
