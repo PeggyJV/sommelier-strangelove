ad9b5aa147fa22b12c19ac02ad254f59ad7d621c
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-03-30 20:32:30 +0700

    feat: add spacing between dots

--- src/components/_tables/StrategySection.tsx
+++ src/components/_tables/StrategySection.tsx
@@ -68,7 +68,7 @@ export const StrategySection: React.FC<StrategySectionProps> = ({
             {date && <StrategyDate date={date} />}
             {provider && strategyType && (
               <Text color="neutral.400">
-                {provider}·{strategyType}
+                {provider} · {strategyType}
               </Text>
             )}
             {netValue && (
