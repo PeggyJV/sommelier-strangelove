ddb641f2149609dcc9c5986cd3c387cae79d5913
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-04-01 03:23:58 +0700

    feat: update timeframe button behaviour

--- src/components/_layout/TimeFrameButton.tsx
+++ src/components/_layout/TimeFrameButton.tsx
@@ -1,33 +1,8 @@
 import { Button, HStack } from "@chakra-ui/react"
 import { useHome } from "data/context/homeContext"
-import { useEffect, useState } from "react"
 
-export const TimeFrameButton = ({
-  containerHeight,
-}: {
-  containerHeight: number
-}) => {
+export const TimeFrameButton = ({ inView }: { inView: boolean }) => {
   const { timeArray, timeline } = useHome()
-  const [isFixed, setIsFixed] = useState(true)
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY
-      const stopPosition = 120
-
-      if (scrollPosition <= stopPosition) {
-        setIsFixed(true)
-      } else {
-        setIsFixed(false)
-      }
-    }
-
-    window.addEventListener("scroll", handleScroll)
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll)
-    }
-  }, [containerHeight])
-
   return (
     <HStack
       w="17rem"
@@ -40,11 +15,7 @@ export const TimeFrameButton = ({
       rounded="100px"
       boxShadow="2xl"
       zIndex={999}
-      position={isFixed ? "fixed" : "unset"}
-      top={{
-        base: isFixed ? "80vh" : containerHeight,
-        lg: isFixed ? "85vh" : containerHeight,
-      }}
+      position={inView ? "unset" : "fixed"}
       bottom={0}
       right={0}
       left={0}
