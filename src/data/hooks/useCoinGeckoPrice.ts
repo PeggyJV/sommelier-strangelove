77cdeb19f44a1788935fa5f7ecc7a1f4ea28750a
Xander Low <xander@xandydandy.com>
2023-04-14 14:50:02 -0700

    changed: retry attempt

--- src/data/hooks/useCoinGeckoPrice.ts
+++ src/data/hooks/useCoinGeckoPrice.ts
@@ -2,8 +2,11 @@ import { useQuery } from "@tanstack/react-query"
 import { fetchCoingeckoPrice } from "queries/get-coingecko-price"
 
 export const useCoinGeckoPrice = (coin: string) => {
-  const query = useQuery(["USE_COIN_GECKO_PRICE", coin], async () => {
-    return await fetchCoingeckoPrice(coin, "usd")
+  const query = useQuery(["USE_COIN_GECKO_PRICE", coin], {
+    queryFn: async () => {
+      return await fetchCoingeckoPrice(coin, "usd")
+    },
+    retry: true,
   })
 
   return query
