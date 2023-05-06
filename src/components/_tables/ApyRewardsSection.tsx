5d028967bd8d7eb0e12a451aebb2f789d08e8f54
Nur Fikri <codingki@gmail.com>
2023-05-06 12:36:27 +0700

    remove one week launch hardcoded apy

--- src/components/_tables/ApyRewardsSection.tsx
+++ src/components/_tables/ApyRewardsSection.tsx
@@ -23,8 +23,7 @@ type ApyRewardsSectionProps = {
 export const ApyRewardsSection: FC<ApyRewardsSectionProps> = (
   props
 ) => {
-  const { baseApy, date, rewardsApy, stackingEndDate, cellarId } =
-    props
+  const { baseApy, rewardsApy, stackingEndDate, cellarId } = props
   const endDate = new Date(stackingEndDate).getTime()
   const startDate = subDays(endDate, 30).getTime()
   const now = new Date(Date.now()).getTime()
@@ -32,10 +31,6 @@ export const ApyRewardsSection: FC<ApyRewardsSectionProps> = (
   const current = now - startDate
   const percentage = (current / range) * 100
   const daysLeft = differenceInDays(endDate, now)
-  const launchDate = new Date(date ?? 0)
-  const numWeeks = 1
-  launchDate.setDate(launchDate.getDate() + numWeeks * 7)
-  const isOneWeekAfterLaunch = new Date() > launchDate
   const cellarConfig = cellarDataMap[cellarId].config
 
   if (!baseApy && !rewardsApy) {
@@ -59,28 +54,9 @@ export const ApyRewardsSection: FC<ApyRewardsSectionProps> = (
         boxShadow="xl"
         shouldWrapChildren
       >
-        {!isOneWeekAfterLaunch ? (
-          <Tooltip
-            label={`Estimated APY`}
-            color="neutral.100"
-            border="0"
-            fontSize="12px"
-            bg="neutral.900"
-            fontWeight={600}
-            py="4"
-            px="6"
-            boxShadow="xl"
-            shouldWrapChildren
-          >
-            <Text textAlign="right" fontWeight={550} fontSize="16px">
-              10%
-            </Text>
-          </Tooltip>
-        ) : (
-          <Text fontWeight={550} fontSize="16px">
-            {baseApy ?? "-"}
-          </Text>
-        )}
+        <Text fontWeight={550} fontSize="16px">
+          {baseApy ?? "-"}
+        </Text>
       </Tooltip>
       {rewardsApy && (
         <Tooltip
