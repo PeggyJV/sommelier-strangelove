8e7973c64692a354b2d179781f94b0473d33b149
asaadam <graha.asa@gmail.com>
2023-05-16 21:39:01 +0700

    fix: revert chart

--- src/components/_charts/UsdcChart.tsx
+++ src/components/_charts/UsdcChart.tsx
@@ -169,54 +169,6 @@ export const UsdcChart: VFC<UsdcChartProps> = ({
       enableArea={true}
       animate={false}
       crosshairType="x"
-      axisBottom={{
-        renderTick: (tick) => {
-          const day = tick.value.getDate()
-          const month = tick.value.toLocaleString("default", {
-            month: "short",
-          })
-          if (timeline === "1W") {
-            return (
-              <g transform={`translate(${tick.x},${tick.y + 20})`}>
-                <text
-                  x={0}
-                  textAnchor="middle"
-                  dominantBaseline="middle"
-                  style={{
-                    fill: "white",
-                    fontSize: 12,
-                    fontWeight: 600,
-                  }}
-                >
-                  {format(new Date(String(tick.value)), "MMM, d")}
-                </text>
-              </g>
-            )
-          }
-          return (
-            <g transform={`translate(${tick.x},${tick.y + 20})`}>
-              <text
-                x={0}
-                textAnchor="middle"
-                dominantBaseline="middle"
-                style={{
-                  fill: "white",
-                  fontSize: 12,
-                  fontWeight: 600,
-                }}
-              >
-                <tspan x="0" dy="0">
-                  {day}
-                </tspan>
-                <tspan x="0" dy="15">
-                  {month}
-                </tspan>
-              </text>
-            </g>
-          )
-        },
-        ...hourlyAxisBottom.axisBottom,
-      }}
       defs={[
         linearGradientDef("gradientA", [
           { offset: 0, color: "inherit" },
