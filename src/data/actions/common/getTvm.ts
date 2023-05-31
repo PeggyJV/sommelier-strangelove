f1c20eb02d1a1776126df4f22f7ebd66ec524798
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-06-01 00:34:36 +0700

    fix: return undefined

--- src/data/actions/common/getTvm.ts
+++ src/data/actions/common/getTvm.ts
@@ -2,7 +2,8 @@ import { formatDecimals } from "utils/bigNumber"
 import { formatCurrency } from "utils/formatCurrency"
 
 export const getTvm = (tvlTotal?: string) => {
-  const calculatedTvl = tvlTotal && formatDecimals(tvlTotal, 18)
+  const calculatedTvl =
+    (tvlTotal && formatDecimals(tvlTotal, 18)) ?? "0"
   const tvmVal = formatCurrency(calculatedTvl)
 
   return {
