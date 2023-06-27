a0bf5463d52e7cb4b4df0ee5573137604ee2b711
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2023-06-27 17:30:51 +0700

    feat: apy rewards section update

--- src/components/_tables/ApyRewardsSection.tsx
+++ src/components/_tables/ApyRewardsSection.tsx
@@ -117,40 +117,18 @@ export const ApyRewardsSection: FC<ApyRewardsSectionProps> = (
     )
   else
     return (
-      <HStack
-        alignItems="center"
-        spacing={0}
-        alignContent="center"
-        gap={2}
-      >
-        <Tooltip
-          label={
-            <>
-              <Text>
-                {baseApyHoverLabel(cellarConfig)} {baseApy ?? "0.00%"}
-              </Text>
-              <Text>Rewards APY {rewardsApy ?? "0.00%"}</Text>
-            </>
-          }
-          color="neutral.100"
-          border="0"
-          fontSize="12px"
-          bg="neutral.900"
-          fontWeight={600}
-          py="4"
-          px="6"
-          boxShadow="xl"
-          shouldWrapChildren
-        >
-          <Text fontWeight={550} fontSize="16px">
-            {baseApySumRewards ?? "-"}
-          </Text>
-        </Tooltip>
-        {rewardsApy && (
+      <Stack alignItems="flex-end">
+        <HStack spacing={0} alignContent="center" gap={2}>
           <Tooltip
-            label={`Rewards ends in ${formatDistanceToNowStrict(
-              new Date(stackingEndDate)
-            )}`}
+            label={
+              <>
+                <Text>
+                  {baseApyHoverLabel(cellarConfig)}{" "}
+                  {baseApy ?? "0.00%"}
+                </Text>
+                <Text>Rewards APY {rewardsApy ?? "0.00%"}</Text>
+              </>
+            }
             color="neutral.100"
             border="0"
             fontSize="12px"
@@ -161,28 +139,48 @@ export const ApyRewardsSection: FC<ApyRewardsSectionProps> = (
             boxShadow="xl"
             shouldWrapChildren
           >
-            <HStack spacing={1}>
-              <CircularProgress
-                value={percentage}
-                color="white"
-                trackColor="none"
-                size="25px"
-              >
-                <CircularProgressLabel
-                  display="flex"
-                  alignItems="center"
-                >
-                  <LogoIcon
-                    mx="auto"
-                    color="red.normal"
-                    p={0}
-                    boxSize="9px"
-                  />
-                </CircularProgressLabel>
-              </CircularProgress>
-            </HStack>
+            <Text fontWeight={550} fontSize="16px">
+              {baseApySumRewards ?? "-"}
+            </Text>
           </Tooltip>
-        )}
-      </HStack>
+          {rewardsApy && (
+            <Tooltip
+              label={`Rewards ends in ${formatDistanceToNowStrict(
+                new Date(stackingEndDate)
+              )}`}
+              color="neutral.100"
+              border="0"
+              fontSize="12px"
+              bg="neutral.900"
+              fontWeight={600}
+              py="4"
+              px="6"
+              boxShadow="xl"
+              shouldWrapChildren
+            >
+              <HStack spacing={1}>
+                <CircularProgress
+                  value={percentage}
+                  color="white"
+                  trackColor="none"
+                  size="25px"
+                >
+                  <CircularProgressLabel
+                    display="flex"
+                    alignItems="center"
+                  >
+                    <LogoIcon
+                      mx="auto"
+                      color="red.normal"
+                      p={0}
+                      boxSize="9px"
+                    />
+                  </CircularProgressLabel>
+                </CircularProgress>
+              </HStack>
+            </Tooltip>
+          )}
+        </HStack>
+      </Stack>
     )
 }
