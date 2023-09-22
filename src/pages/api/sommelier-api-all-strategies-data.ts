991952935ecd3899b5dd79366e1ff1d1a6cb988b
Phil <pbal@fordham.edu>
2023-09-22 15:35:48 -0600

    Change tvl endpoint in all strat data + remove unused code

--- src/pages/api/sommelier-api-all-strategies-data.ts
+++ src/pages/api/sommelier-api-all-strategies-data.ts
@@ -37,7 +37,7 @@ const sommelierAPIAllStrategiesData = async (
 
     // TODO: Generalize for multichain
     let allStrategyData = `https://api.sommelier.finance/dailyData/ethereum/allCellars/${monthAgoEpoch}/latest`
-    let tvlData = `${baseUrl}api/sommelier-api-tvl` // Hit local endpoint for cached data
+    let tvlData = `https://api.sommelier.finance/tvl`
 
     const [allStrategyDataResponse, tvlDataResponse] =
       await Promise.all([
@@ -89,7 +89,7 @@ const sommelierAPIAllStrategiesData = async (
       transformedData.sort((a: any, b: any) => b.date - a.date)
       
       // Get tvl
-      let tvl = fetchedTVL.result.data[cellarAddress]
+      let tvl = fetchedTVL.Response[cellarAddress]
 
       if (tvl === undefined) {
         tvl = 0
