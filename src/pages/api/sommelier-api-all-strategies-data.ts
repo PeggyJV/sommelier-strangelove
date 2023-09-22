71f149f3e4c71aa0f9ac894a402dd952c429aefb
Phil <pbal@fordham.edu>
2023-09-22 15:40:07 -0600

    remove /

--- src/pages/api/sommelier-api-all-strategies-data.ts
+++ src/pages/api/sommelier-api-all-strategies-data.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { CellaAddressDataMap } from "data/cellarDataMap"
 
 const baseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/"
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
 
 interface CellarType {
   id: string
