9a74d391b0da79c92ccf26f7df958ed670ef8530
your_username <henriots33@gmail.com>
2023-10-10 23:23:19 +0300

    added-balancer

--- src/data/strategies/turbo-steth.ts
+++ src/data/strategies/turbo-steth.ts
@@ -21,7 +21,7 @@ export const turboSTETH: CellarData = {
   managementFee: "1.00%",
   managementFeeTooltip:
     "An annual charge on your deposited amount for the pro-rated period during which your deposit remains in the vault.",
-  protocols: ["AAVE", "Morpho", "Uniswap V3"],
+  protocols: ["AAVE", "Morpho", "Uniswap V3", "Balancer"],
   strategyAssets: ["WETH", "stETH", "wstETH"],
   performanceSplit: {
     depositors: 80,
