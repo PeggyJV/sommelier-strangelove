f64b8bd52dadc793404928ddaa48464014f74382
Phil <pbal@fordham.edu>
2023-11-11 17:30:11 -0700

    Abstract 'AllContracts' to use multichain as needed

--- src/data/hooks/useAllContracts.ts
+++ src/data/hooks/useAllContracts.ts
@@ -1,16 +1,29 @@
 import { useQuery } from "@tanstack/react-query"
 import { getAllContracts } from "data/actions/common/getAllContracts"
-import { useProvider, useSigner } from "wagmi"
+import { useSigner, useProvider } from "wagmi"
+import { chainConfig } from "data/chainConfig"
 
 export const useAllContracts = () => {
   const { data: signer } = useSigner()
   const provider = useProvider()
+
+  // Prepare the providers and signers map
+  const providerMap = new Map()
+  const signerMap = new Map()
+
+  chainConfig.forEach((chain) => {
+    providerMap.set(chain.id, provider)
+    signerMap.set(chain.id, signer)
+  })
+
   const query = useQuery(
-    [
-      "USE_ALL_STRATEGIES_CONTRACTS",
-      { signer: signer?._isSigner, provider: provider?._isProvider },
-    ],
-    getAllContracts
+    ["USE_ALL_STRATEGIES_CONTRACTS"],
+    () => getAllContracts(providerMap, signerMap),
+    {
+      // If you want to enable/disable this query based on the presence of provider/signer
+      enabled: !!provider && !!signer,
+    }
   )
+
   return query
 }
