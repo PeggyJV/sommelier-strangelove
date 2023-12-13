9680262f652f4da170b10280b831d89915fd1696
Phil <pbal@fordham.edu>
2023-12-12 18:10:40 -0700

    Move filter to its own file for cleanliness

--- src/components/_pages/PageHome.tsx
+++ src/components/_pages/PageHome.tsx
@@ -1,19 +1,4 @@
-import {
-  Button,
-  Center,
-  HStack,
-  Image,
-  Text,
-  Stack,
-  Box,
-  Popover,
-  PopoverTrigger,
-  PopoverBody,
-  PopoverContent,
-  AvatarGroup,
-  Avatar,
-  Checkbox,
-} from "@chakra-ui/react"
+import { Button, Center } from "@chakra-ui/react"
 import { ErrorCard } from "components/_cards/ErrorCard"
 import { StrategyDesktopColumn } from "components/_columns/StrategyDesktopColumn"
 import { StrategyMobileColumn } from "components/_columns/StrategyMobileColumn"
@@ -33,12 +18,8 @@ import {
 import useBetterMediaQuery from "hooks/utils/useBetterMediaQuery"
 import { useMemo, useState } from "react"
 import { InfoBanner } from "components/_banners/InfoBanner"
-import {
-  chainConfig,
-  Chain,
-  chainConfigMap,
-} from "src/data/chainConfig"
-import { ChevronDownIcon } from "components/_icons"
+import { ChainFilter } from "components/_filters/ChainFilter"
+import { chainConfig } from "src/data/chainConfig"
 
 export const PageHome = () => {
   const {
@@ -52,10 +33,6 @@ export const PageHome = () => {
   const isMobile = useBetterMediaQuery("(max-width: 900px)")
   const isTab = useBetterMediaQuery("(max-width: 1600px)")
   const isDesktop = !isTab && !isMobile
-
-  const allChainIds = chainConfig.map((chain) => chain.id)
-  const [selectedChainIds, setSelectedChainIds] =
-    useState<string[]>(allChainIds)
 
   const {
     isOpen,
@@ -114,34 +91,10 @@ export const PageHome = () => {
         },
       })
 
-  const handleChainClick = (chainId: string) => {
-    setSelectedChainIds((current) => {
-      const normalizedChainId = chainId.toLowerCase()
+  const allChainIds = chainConfig.map((chain) => chain.id)
 
-      if (current.includes(normalizedChainId)) {
-        // If the chain is already selected, remove it from the array
-        return current.filter((id) => id !== normalizedChainId)
-      } else {
-        // If the chain is not selected, add it to the array
-        return [...current, normalizedChainId]
-      }
-    })
-  }
-
-  const [checkedStates, setCheckedStates] = useState(
-    new Map(chainConfig.map((chain) => [chain.id, true])) // Default all chains to checked
-  )
-
-  const toggleCheck = (id: string) => {
-    setCheckedStates((prev) => {
-      const newCheckedStates = new Map(prev)
-      newCheckedStates.set(
-        id.toLowerCase(),
-        !newCheckedStates.get(id.toLowerCase())
-      )
-      return newCheckedStates
-    })
-  }
+  const [selectedChainIds, setSelectedChainIds] =
+    useState<string[]>(allChainIds)
 
   const strategyData = useMemo(() => {
     return (
@@ -192,131 +145,12 @@ export const PageHome = () => {
           </Text>
         </VStack>
       </HStack> */}
-      <HStack mb="1.6rem">
-        <HStack spacing="8px">
-          <Popover placement="bottom">
-            <PopoverTrigger>
-              <Button
-                bg="none"
-                borderWidth={2.5}
-                borderColor="purple.base"
-                borderRadius="1em"
-                rightIcon={<ChevronDownIcon />}
-                w="auto"
-                zIndex={401}
-                fontFamily="Haffer"
-                fontSize={12}
-                padding="1.75em 2em"
-                _hover={{
-                  bg: "purple.dark",
-                }}
-                leftIcon={
-                  <HStack>
-                    <Text fontSize={"1.25em"}>Networks</Text>
-                    <HStack justifyContent={"center"}>
-                      <AvatarGroup size="sm" dir="reverse">
-                        {selectedChainIds
-                          .slice(0, 5)
-                          .map((chainStr: String) => {
-                            const chain =
-                              chainConfigMap[chainStr.toLowerCase()]
-                            return (
-                              <Avatar
-                                name={chain.displayName}
-                                src={chain.logoPath}
-                                key={chain.id}
-                              />
-                            )
-                          })}
-                      </AvatarGroup>
-                      {selectedChainIds.length > 5 && (
-                        <Text fontWeight={600}>
-                          +{selectedChainIds.length - 5}
-                        </Text>
-                      )}
-                    </HStack>
-                    {selectedChainIds.length > 5 && (
-                      <Text fontSize="sm">
-                        +{selectedChainIds.length - 5}
-                      </Text>
-                    )}
-                  </HStack>
-                }
-              />
-            </PopoverTrigger>
-
-            <PopoverContent
-              p={2}
-              maxW="max-content"
-              borderWidth={1}
-              borderColor="purple.dark"
-              borderRadius={2}
-              bg="surface.bg"
-              fontWeight="semibold"
-              _focus={{
-                outline: "unset",
-                outlineOffset: "unset",
-                boxShadow: "unset",
-              }}
-            >
-              <PopoverBody p={0}>
-                <Stack>
-                  {chainConfig.map((chain: Chain) => {
-                    const isSelected = selectedChainIds.includes(
-                      chain.id
-                    )
-                    return (
-                      <Box
-                        as="button"
-                        key={chain.id}
-                        py={2}
-                        px={4}
-                        fontSize="sm"
-                        borderRadius={6}
-                        onClick={() => {
-                          handleChainClick(chain.id)
-                          toggleCheck(chain.id)
-                        }}
-                        _hover={{
-                          cursor: "pointer",
-                          bg: "purple.dark",
-                          borderColor: "surface.tertiary",
-                        }}
-                      >
-                        <HStack
-                          display="flex" // Use flex display
-                          justifyContent="space-between" // Space between items
-                          alignItems="center" // Align items vertically
-                          width="100%" // Full width
-                          spacing={3}
-                        >
-                          <Image
-                            src={chain.logoPath}
-                            alt={chain.displayName}
-                            boxSize="24px"
-                          />
-                          <Text fontWeight="semibold">
-                            {chain.displayName}
-                          </Text>{" "}
-                          <Checkbox
-                            id={chain.id}
-                            defaultChecked={true}
-                            isChecked={checkedStates.get(chain.id)}
-                            onChange={(e) => {
-                              handleChainClick(chain.id)
-                              toggleCheck(chain.id)
-                            }}
-                          />
-                        </HStack>
-                      </Box>
-                    )
-                  })}
-                </Stack>
-              </PopoverBody>
-            </PopoverContent>
-          </Popover>
-        </HStack>
-      </HStack>
+      <ChainFilter
+        {...{
+          selectedChainIds,
+          setSelectedChainIds,
+        }}
+      />
       <TransparentSkeleton
         height={loading ? "400px" : "auto"}
         w="full"
