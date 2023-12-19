acde796773d9c9f6fe828f219ceb6c046d542910
your_username <henriots33@gmail.com>
2023-12-19 19:05:09 +0100

    remove define logic

--- src/data/strategies/real-yield-uni.ts
+++ src/data/strategies/real-yield-uni.ts
@@ -29,7 +29,7 @@ export const realYieldUNI: CellarData = {
   },
   strategyProvider: {
     logo: "/assets/images/seven-seas.png",
-    title: "Seven Seas & DeFine Logic Labs",
+    title: "Seven Seas",
     href: "https://sevenseas.capital/",
     tooltip:
       "A Strategy Provider is responsible for providing the instructions for a cellar to execute",
