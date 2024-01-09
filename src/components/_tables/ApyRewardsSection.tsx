dcf4d853515b92973884a354c6b3baf6131201a7
henrio123 <henriots33@gmail.com>
2024-01-09 16:52:56 +0200

    update

--- src/components/_tables/ApyRewardsSection.tsx
+++ src/components/_tables/ApyRewardsSection.tsx
@@ -351,7 +351,7 @@ export const ApyRewardsSection: FC<ApyRewardsSectionProps> = (
                       color="red.normal"
                       p={0}
                       boxSize={
-                        cellarConfig.customReward?.logoSize ?? "9px"
+                        cellarConfig.customReward2?.logoSize2 ?? "9px"
                       }
                     />
                   </CircularProgressLabel>
