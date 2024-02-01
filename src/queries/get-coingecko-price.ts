cf9dc1382a9e9ed7c9836403ae90cb6db12c495e
aleots <aleots@ttu.ee>
2024-02-01 09:08:20 +0200

    Renaming, fix fetching price from contract

--- src/queries/get-coingecko-price.ts
+++ src/queries/get-coingecko-price.ts
@@ -1,7 +1,7 @@
 import { queryContract } from "context/rpc_context"
 import { chainConfigMap } from "data/chainConfig"
-import pricerouterAbi from "src/abi/pricerouterAbi.json"
-import { Contract } from "ethers"
+import { tokenConfig } from "data/tokenConfig"
+import priceRouterAbi from "src/abi/price-router.json"
 
 const getUrl = (baseId: string, quoteId: string) =>
   `/api/coingecko-simple-price?base=${baseId}&quote=${quoteId}`
@@ -18,23 +18,38 @@ export const fetchCoingeckoPrice = async (
     const data = await fetch(url)
     const result = await data.json()
 
-    return result.price ? result.price + "" : undefined
+    if (result.price === undefined) {
+      return await fetchPriceFromContract(base)
+    }
+
+    return result.price + ""
   } catch (error) {
-    console.log("Error fetching Coingecko Price", error)
-    return getPriceFromEtherScan(base)
+    console.log("Error fetching Price", error)
+    throw Error(error as string)
   }
 }
 
-export const getPriceFromEtherScan = async (base: string) => {
+const fetchPriceFromContract = async (base: string) => {
   const contractAddress = "0xA1A0bc3D59e4ee5840c9530e49Bdc2d1f88AaF92"
-  const contract = (await queryContract(
+  const token = tokenConfig.find(token => token.coinGeckoId === base)
+
+  if(token === undefined){
+    throw Error("Token is undefined")
+  }
+
+  const chain = chainConfigMap[token.chain]
+  const tokenAddress = token.address
+
+  const contract = await queryContract(
     contractAddress,
-    pricerouterAbi,
-    chainConfigMap["ethereum"]
-  )) as unknown as SomeContract
-  const priceInUSD = await contract.getPriceInUSD(base)
+    priceRouterAbi,
+    chain
+  );
+
+  if(contract === null){
+    throw Error("Contract is null")
+  }
+  const priceInUSD = await contract.getPriceInUSD(tokenAddress)
+
   return (priceInUSD / 1e8).toString()
-}
-interface SomeContract extends Contract {
-  getPriceInUSD(base: string): Promise<number>
 }
