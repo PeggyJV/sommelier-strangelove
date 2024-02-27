8d0cf96c7139b8ee6489f2f6ed8bbf7615fc27e6
henrio123 <henriots33@gmail.com>
2024-02-27 20:24:31 +0200

    src-data-cellarDataMap

--- src/data/cellarDataMap.ts
+++ src/data/cellarDataMap.ts
@@ -42,6 +42,7 @@ cellarConfig = {
   // ! NOTE THIS DETERMINES INITIAL ORDERING
   //[config.CONTRACT.TEST_ARBITRUM_REAL_YIELD_USD.SLUG]:testArbitrumRealYieldUsd, //! Multichain testing contract
   [config.CONTRACT.TURBO_EZETH.SLUG]: turboezETH,
+  [config.CONTRACT.TURBO_RSETH.SLUG]: turborsETH,
   [config.CONTRACT.REAL_YIELD_USD_ARB.SLUG]: realYieldUsdArb,
   [config.CONTRACT.REAL_YIELD_ETH_ARB.SLUG]: realYieldEthArb,
   [config.CONTRACT.TURBO_EETHV2.SLUG]: turboeETHV2,
@@ -79,6 +80,7 @@ cellarConfig = {
 // ! For not ethereum cellars the key must be {cellarAddress}-{chainId}
 let cellarAddressConfig: CellarAddressDataMap
 cellarAddressConfig = {
+  [config.CONTRACT.TURBO_RSETH.ADDRESS.toLowerCase()]: turborsETH,
   [config.CONTRACT.TURBO_EZETH.ADDRESS.toLowerCase()]: turboezETH,
   [config.CONTRACT.REAL_YIELD_USD_ARB.ADDRESS.toLowerCase() +
   "-arbitrum"]: realYieldUsdArb,
