9fea01d9e91d52a6bdd0836bc62f632212f134b4
henrio123 <henriots33@gmail.com>
2024-03-20 16:05:20 +0200

    added pubKey

--- src/pages/api/saveSignedMessage.ts
+++ src/pages/api/saveSignedMessage.ts
@@ -1,6 +1,6 @@
 //src/pages/api/saveSignedMessage.ts
 import { NextApiRequest, NextApiResponse } from "next"
-import { kv } from "@vercel/kv" /
+import { kv } from "@vercel/kv" 
 import { verifyADR36Amino } from "@keplr-wallet/cosmos/build/adr-36/amino"
 
 export default async function handler(
