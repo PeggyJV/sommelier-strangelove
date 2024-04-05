d01b3777aabaf0046e8784911efbe8868f134af0
henrio123 <henriots33@gmail.com>
2024-04-05 12:30:44 +0300

    fix do not load snapshot modal with everyload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
 import type { NextPage } from "next"
 import { PageHome } from "components/_pages/PageHome"
+import { useState, useEffect } from "react"
+import { SnapshotNotifyModal } from "components/_modals/SnapshotNotifyModal."
 
 const Home: NextPage = () => {
-  return <PageHome />
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  useEffect(() => {
+    const modalShown = localStorage.getItem("modalShown")
+    if (!modalShown) {
+      setIsModalOpen(true)
+      localStorage.setItem("modalShown", "true")
+    }
+  }, [])
+
+  const handleCloseModal = () => setIsModalOpen(false)
+
+  return (
+    <>
+      <PageHome />
+      {isModalOpen && (
+        <SnapshotNotifyModal
+          isOpen={isModalOpen}
+          onClose={handleCloseModal}
+        />
+      )}
+    </>
+  )
 }
 
 export default Home
