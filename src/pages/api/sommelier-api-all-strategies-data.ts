ba3f37d8a6cd766049e9c8371db9e21ca5ae97c8
henrio123 <henriots33@gmail.com>
2024-04-17 15:00:50 +0300

    sommelier-api-all-strategies-data.ts

--- src/pages/api/sommelier-api-all-strategies-data.ts
+++ src/pages/api/sommelier-api-all-strategies-data.ts
@@ -41,17 +41,20 @@ const sommelierAPIAllStrategiesData = async (
     let allEthereumStrategyData = `https://api.sommelier.finance/dailyData/ethereum/allCellars/${monthAgoEpoch}/latest`
     let allArbitrumStrategyData = `https://api.sommelier.finance/dailyData/arbitrum/allCellars/${monthAgoEpoch}/latest`
     let allOptimismStrategyData = `https://api.sommelier.finance/dailyData/optimism/allCellars/${monthAgoEpoch}/latest`
+    // let allScrollStrategyData = `https://api.sommelier.finance/dailyData/scroll/allCellars/${monthAgoEpoch}/latest`
     let tvlData = `https://api.sommelier.finance/tvl`
 
     const [
       allEthereumStrategyDataResponse,
       allArbitrumStrategyDataResponse,
       allOptimismStrategyDataResponse,
+      // allScrollStrategyDataResponse,
       tvlDataResponse,
     ] = await Promise.all([
       fetchData(allEthereumStrategyData),
       fetchData(allArbitrumStrategyData),
       fetchData(allOptimismStrategyData),
+      // fetchData(allScrollStrategyData),
       fetchData(tvlData),
     ])
 
@@ -59,6 +62,7 @@ const sommelierAPIAllStrategiesData = async (
       allEthereumStrategyDataResponse.status !== 200 ||
       allArbitrumStrategyDataResponse.status !== 200 ||
       allOptimismStrategyDataResponse.status !== 200 ||
+      // allScrollStrategyDataResponse.status !== 200 ||
       tvlDataResponse.status !== 200
     ) {
       throw new Error(
@@ -68,6 +72,8 @@ const sommelierAPIAllStrategiesData = async (
           allArbitrumStrategyDataResponse.status +
           " Optimism Strategy Response code: " +
           allOptimismStrategyDataResponse.status +
+          // " Scroll Strategy Response code: " +
+          // allScrollStrategyDataResponse.status +
           " Tvl Response code:" +
           tvlDataResponse.status
       )
@@ -79,6 +85,8 @@ const sommelierAPIAllStrategiesData = async (
       await allArbitrumStrategyDataResponse.json()
     const fetchedOptimismData =
       await allOptimismStrategyDataResponse.json()
+    // const fetchedScrollData =
+    //   await allScrollStrategyDataResponse.json()  
 
     let returnObj = {
       result: {
@@ -242,6 +250,57 @@ const sommelierAPIAllStrategiesData = async (
         returnObj.result.data.cellars.push(cellarObj)
       }
     )
+
+        // ! Scroll transform
+        // Object.keys(fetchedOptimismData.Response).forEach(
+        //   (cellarAddress) => {
+        //     // If the cellar address is not in the CellaAddressDataMap skip it
+        //     if (
+        //       CellaAddressDataMap[
+        //         cellarAddress!.toString().toLowerCase() + "-scroll"
+        //       ] === undefined
+        //     ) {
+        //       console.warn(`${cellarAddress} not a valid cellar address`)
+        //       return
+        //     }
+    
+        //     let cellarDecimals =
+        //       CellaAddressDataMap[
+        //         cellarAddress!.toString().toLowerCase() + "-scroll"
+        //       ].config.cellar.decimals
+    
+        //     let transformedData = fetchedOptimismData.Response[
+        //       cellarAddress
+        //     ].map((dayData: any) => ({
+        //       date: dayData.unix_seconds,
+        //       // Multiply by cellarDecimals and drop any decimals
+        //       shareValue: Math.floor(
+        //         dayData.share_price * 10 ** cellarDecimals
+        //       ).toString(),
+        //     }))
+    
+        //     // Order by descending date
+        //     transformedData.sort((a: any, b: any) => b.date - a.date)
+    
+        //     // Get tvl
+        //     let tvl = fetchedTVL.Response[cellarAddress + "-scroll"]
+    
+        //     if (tvl === undefined) {
+        //       tvl = 0
+        //     }
+    
+        //     // Create a new response object with the transformed data
+        //     let cellarObj = {
+        //       id: cellarAddress.toLowerCase() + "-scroll",
+        //       dayDatas: transformedData,
+        //       shareValue: transformedData[0].shareValue,
+        //       tvlTotal: tvl,
+        //       chain: chainSlugMap.SCROLL.id,
+        //     }
+    
+        //     returnObj.result.data.cellars.push(cellarObj)
+        //   }
+        // )
 
     res.setHeader(
       "Cache-Control",
