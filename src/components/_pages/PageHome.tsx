ff5ebe5b3a47ec954cda0cded41ffc65500e9b26
henrio123 <henriots33@gmail.com>
2024-04-23 11:29:09 +0300

    remove-infobanner

--- src/components/_pages/PageHome.tsx
+++ src/components/_pages/PageHome.tsx
@@ -294,22 +294,15 @@ export const PageHome = () => {
   const loading = isFetching || isRefetching || isLoading
   return (
     <LayoutWithSidebar>
-      <InfoBanner
-        text={
-          <Link href="/snapshot" passHref>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a
-              style={{
-                textDecoration: "underline",
-                color: "inherit",
-              }}
-            >
-              Unlock RedStone RSG Points: Act Now Before They're Gone!
-            </a>
-          </Link>
-        }
-      />
-
+      {/*
+        <InfoBanner
+          text={
+            "A new SOMM incentive proposal for Real Yield ETH on Arbitrum is progressing through governance. If it passes, rewards will begin on March 17."
+            "New incentive programs for Real Yield ETH (on Ethereum) Real Yield BTC and Real Yield USD (on Arbitrum) are progressing through governance. If they pass, rewards will begin flowing on March 24"
+          }
+        />
+      }
+      */}
       {/* <HStack
         p={4}
         mb={6}
