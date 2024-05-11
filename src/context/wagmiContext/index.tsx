725298ceb7239d6ead3f1c007a45bbe1f068ed02
henrio123 <henriots33@gmail.com>
2024-05-11 08:21:09 +0300

    TanStack Query as a peer dependency and update the import statements according to the new package structure

--- src/context/wagmiContext/index.tsx
+++ src/context/wagmiContext/index.tsx
@@ -1,66 +1,73 @@
-import { ReactNode } from "react"
-import { mainnet, arbitrum, optimism } from "wagmi/chains"
-import {
-  injected,
-  walletConnect,
-  coinbaseWallet,
-} from "wagmi/connectors"
-import { createClient } from 'viem'
-import { WagmiProvider as WagmiConfig } from "wagmi"
-import { http } from "viem"
+// src/context/wagmiContext/index.tsx
+import { ReactNode } from "react";
+import { mainnet, configureChains, createClient, WagmiConfig } from "wagmi";
+import { arbitrum, optimism } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { infuraProvider } from "wagmi/providers/infura";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { WalletConnectLegacyConnector } from "wagmi/connectors/walletConnectLegacy";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_KEY
-const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY
+const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
+const WALLETCONNECT_PROJECT_ID = process.env.NEXT_WALLETCONNECT_PROJECT_ID;
+const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY;
 
-const provider = http(
-  `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`
-)
-const webSocketProvider = http(
-  `https://mainnet.infura.io/v3/${INFURA_API_KEY}`
-)
+const queryClient = new QueryClient();
 
-const connectors = () => [
-  walletConnect({
-    chains: [mainnet, arbitrum, optimism],
-    options: {
-      projectId:
-        process.env.NEXT_WALLETCONNECT_PROJECT_ID ||
-        "c11d8ffaefb8ba4361ae510ed7690cb8",
-      showQrModal: true,
-    },
-  }),
-  injected({
-    chains: [mainnet, arbitrum, optimism],
-    options: { shimDisconnect: true },
-  }),
-  coinbaseWallet({
-    chains: [mainnet, arbitrum, optimism],
-    options: {
-      appName: "Sommelier",
-      darkMode: true,
-    },
-  }),
-]
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet, arbitrum, optimism],
+  [
+    alchemyProvider({ apiKey: ALCHEMY_API_KEY! }),
+    infuraProvider({
+      apiKey: INFURA_API_KEY!,
+    }),
+    publicProvider(),
+  ],
+  { targetQuorum: 1 }
+);
 
-let client
-try {
-  client = createClient({
-    autoConnect: true,
-    provider,
-    connectors: connectors(),
-    webSocketProvider,
-  })
-} catch (error) {
-  console.error("Error creating Wagmi client:", error)
-}
+const connectors = () => {
+  console.log("WALLETCONNECT_PROJECT_ID", WALLETCONNECT_PROJECT_ID);
+  return [
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: WALLETCONNECT_PROJECT_ID || "c11d8ffaefb8ba4361ae510ed7690cb8",
+        showQrModal: true,
+      },
+    }),
+    new MetaMaskConnector({ chains }),
+    new InjectedConnector({
+      chains,
+      options: { shimDisconnect: true },
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "Sommelier",
+        darkMode: true,
+      },
+    }),
+  ];
+};
 
-export const WagmiProvider = ({
-  children,
-}: {
-  children: ReactNode
-}) => {
-  if (!client) {
-    return <div>Loading...</div> // Or handle the error as you see fit
-  }
-  return <WagmiConfig client={client}>{children}</WagmiConfig>
-}
+const client = createClient({
+  autoConnect: true,
+  provider,
+  connectors: connectors(),
+  webSocketProvider,
+});
+
+export const WagmiProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WagmiConfig client={client}>
+        {children}
+      </WagmiConfig>
+    </QueryClientProvider>
+  );
+};
