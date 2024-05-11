85454ff463564cb8abda3377702f6f4a53df477d
henrio123 <henriots33@gmail.com>
2024-05-11 10:31:40 +0300

    Remove configureChains

--- src/context/wagmiContext/index.tsx
+++ src/context/wagmiContext/index.tsx
@@ -1,54 +1,48 @@
 import { ReactNode } from "react";
-import { mainnet, configureChains, createClient, WagmiConfig, chain } from "wagmi";
-import { arbitrum, optimism } from "wagmi/chains";
-import { http } from 'viem';  // Import http transport from Viem
+import { createClient, http } from 'viem';  // Import http transport from Viem
 import { injected, walletConnect, coinbaseWallet, metaMask } from 'wagmi/connectors';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { arbitrum, mainnet, optimism } from "wagmi/chains";
+import { WagmiConfig } from "wagmi";
 
 const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
 const WALLETCONNECT_PROJECT_ID = process.env.NEXT_WALLETCONNECT_PROJECT_ID;
-const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY;
 
 const queryClient = new QueryClient();
 
+// HTTP transports for each chain
 const alchemyTransport = http(`https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`);
-const infuraTransport = http(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`);
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, arbitrum, optimism],
-  [alchemyTransport, infuraTransport],
-  { targetQuorum: 1 }
-);
-
-const connectors = () => {
-  return [
+// Creating the client directly with chain and transport configurations
+const client = createClient({
+  autoConnect: true,
+  chains: [mainnet, arbitrum, optimism],
+  transports: {
+    [mainnet.id]: alchemyTransport,
+    [arbitrum.id]: alchemyTransport, 
+    [optimism.id]: alchemyTransport 
+  },
+  connectors: () => [
     walletConnect({
-      chains,
+      chains: [mainnet, arbitrum, optimism],
       options: {
         projectId: WALLETCONNECT_PROJECT_ID || "c11d8ffaefb8ba4361ae510ed7690cb8",
         qrcode: true,
       },
     }),
-    metaMask({ chains }),
+    metaMask({ chains: [mainnet, arbitrum, optimism] }),
     injected({
-      chains,
+      chains: [mainnet, arbitrum, optimism],
       options: { shimDisconnect: true },
     }),
     coinbaseWallet({
-      chains,
+      chains: [mainnet, arbitrum, optimism],
       options: {
         appName: "Sommelier",
         darkMode: true,
       },
     }),
-  ];
-};
-
-const client = createClient({
-  autoConnect: true,
-  provider,
-  connectors: connectors(),
-  webSocketProvider,
+  ]
 });
 
 export const WagmiProvider = ({ children }: { children: ReactNode }) => {
