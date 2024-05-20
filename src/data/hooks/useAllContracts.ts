9c4f5aad80d6e622aa7a872278e7150ee3cd7914
henrio123 <henriots33@gmail.com>
2024-05-20 13:01:37 +0300

    in src/data/hooks/useAllStrategiesData.ts Changes Made: Imported usePublicClient from wagmi: import { useAccount, usePublicClient } from "wagmi" Replaced the useProvider hook with usePublicClient: const client = usePublicClient() Updated the queryKey to use client?.uid instead of provider!.uid: queryKey: ["USE_ALL_STRATEGIES_DATA", client?.uid ]

--- src/data/hooks/useAllContracts.ts
+++ src/data/hooks/useAllContracts.ts
@@ -1,26 +1,26 @@
 // sommelier-strangelove/src/data/hooks/useAllContracts.ts
 import { useQuery } from "@tanstack/react-query"
 import { getAllContracts } from "data/actions/common/getAllContracts"
-import { useSigner, useProvider } from "wagmi"
+import { useSigner, usePublicClient } from "wagmi"
 import { chainConfig } from "data/chainConfig"
 import { ethers } from "ethers"
 import { INFURA_API_KEY } from "src/context/rpc_context"
 
 export const useAllContracts = () => {
   const { data: signer } = useSigner()
-  const provider = useProvider()
+  const publicClient = usePublicClient()
 
   // Prepare the providers and signers map
   const providerMap = new Map()
   const signerMap = new Map()
 
   // Current chain
-  let chainId = provider.network.chainId
+  let chainId = publicClient.network.chainId
 
   chainConfig.forEach((chain) => {
     // Only set provider for the current chain
     if (chain.wagmiId !== chainId) {
-      // Create a new provider for the non current chain
+      // Create a new provider for the non-current chain
       providerMap.set(
         chain.id,
         new ethers.providers.JsonRpcProvider(
@@ -29,7 +29,7 @@ export const useAllContracts = () => {
       )
     } else {
       // Only set signer for the current chain
-      providerMap.set(chain.id, provider)
+      providerMap.set(chain.id, publicClient)
       signerMap.set(chain.id, signer)
     }
   })
@@ -37,7 +37,7 @@ export const useAllContracts = () => {
   const query = useQuery({
     queryKey: [
       "USE_ALL_STRATEGIES_CONTRACTS",
-      { signer: signer?._isSigner, provider: provider?._isProvider },
+      { signer: signer?._isSigner, provider: publicClient?._isProvider },
     ],
     queryFn:() => getAllContracts(providerMap, signerMap)
   }
