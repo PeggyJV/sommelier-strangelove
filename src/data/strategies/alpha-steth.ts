eb7a0125f52357237c5540cd22850bb8f470ffd2
otsalex <alexots13@gmail.com>
2025-04-22 17:32:23 +0300

    Native ETH deposit

--- src/data/strategies/alpha-steth.ts
+++ src/data/strategies/alpha-steth.ts
@@ -63,7 +63,7 @@ export const alphaSteth: CellarData = {
   dashboard:
     "https://debank.com/profile/0xec797d184a8c6cd1ef01fadc98b575e84b156c18",
   depositTokens: {
-    list: ["WETH", "stETH", "wstETH"],
+    list: ["WETH", "stETH", "wstETH", "ETH"],
   },
   config: {
     chain: chainSlugMap.ETHEREUM,
