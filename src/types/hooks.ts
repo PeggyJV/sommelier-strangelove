072a93b4c878409563cb5c1f10e6a02c91c456cf
otsalex <alexots13@gmail.com>
2025-06-04 11:41:19 +0300

    Package errors

--- src/types/hooks.ts
+++ src/types/hooks.ts
@@ -2,7 +2,7 @@ export interface MutationEventArgs<
   TInitial = unknown,
   TSuccess = TInitial
 > {
-  onError?: (error: unknown, data: TInitial) => unknown
+  onError?:  (err: unknown, variables: unknown, context?: unknown) => Promise<void> | void
   onMutate?: (data: TInitial) => unknown
-  onSuccess?: (data: TSuccess) => unknown
+  onSuccess?: (data: TSuccess, variables: unknown, context: unknown) => Promise<void> | void
 }
