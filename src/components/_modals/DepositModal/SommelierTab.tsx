d3e931f4492d3169e7e089cff6ccd3695fb7860e
henrio123 <henriots889@gmail.com>
2025-08-17 23:51:15 +0300

    feat: add Alpha-stETH vault logo to import token functionality

--- src/components/_modals/DepositModal/SommelierTab.tsx
+++ src/components/_modals/DepositModal/SommelierTab.tsx
@@ -532,6 +532,7 @@ export const SommelierTab = ({
                     importToken.mutate({
                       address: cellarAddress,
                       chain: cellarConfig.chain.id,
+                      imageUrl: cellarConfig.lpToken.imagePath,
                     })
                   }}
                   textDecor="underline"
