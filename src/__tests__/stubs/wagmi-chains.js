1babcf78b219a6dc70e52dea7efd79595fb3d3fe
henri ots <henriots889@gmail.com>
2025-08-27 14:30:14 +0300

    test: add blockExplorers to wagmi chains stub; use group role for disabled assertions; jest clear/restore mocks

--- src/__tests__/stubs/wagmi-chains.js
+++ src/__tests__/stubs/wagmi-chains.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mainnet: { id: 1, name: 'Ethereum' },
-  arbitrum: { id: 42161, name: 'Arbitrum One' },
-  optimism: { id: 10, name: 'OP Mainnet' },
+  mainnet: { id: 1, name: "Ethereum", blockExplorers: { default: { url: "https://etherscan.io" } } },
+  arbitrum: { id: 42161, name: "Arbitrum One", blockExplorers: { default: { url: "https://arbiscan.io" } } },
+  optimism: { id: 10, name: "OP Mainnet", blockExplorers: { default: { url: "https://optimistic.etherscan.io" } } },
 }
-
-
